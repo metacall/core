8161e4a5ccfc96efac8c712d620179b481a6e58f
Vicente Ferrer <vic798@gmail.com>
2016-10-29 10:55:49 +0200

    Solved minor bug in py_loader_test, initialization of logger not available.

--- source/tests/py_loader_test/source/py_loader_test.cpp
+++ source/tests/py_loader_test/source/py_loader_test.cpp
@@ -10,6 +10,8 @@
 
 #include <loader/loader.h>
 
+#include <log/log.h>
+
 class py_loader_test : public testing::Test
 {
   protected:
@@ -18,6 +20,12 @@ class py_loader_test : public testing::Test
 TEST_F(py_loader_test, DefaultConstructor)
 {
 	loader_naming_name name = "example.py";
+
+	EXPECT_EQ((int) 0, (int) log_configure("metacall",
+		log_policy_format_text(),
+		log_policy_schedule_sync(),
+		log_policy_storage_sequential(),
+		log_policy_stream_stdio(stdout)));
 
 	EXPECT_EQ((int) 0, loader_load(name));
 
