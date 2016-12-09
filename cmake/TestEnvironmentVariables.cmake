a2533064f893dce371f9b615fe0f30e26577baac
Vicente Ferrer <vic798@gmail.com>
2016-12-09 21:57:33 +0100

    Added debug environment variables for old versions of Visual Studio.

--- cmake/TestEnvironmentVariables.cmake
+++ cmake/TestEnvironmentVariables.cmake
@@ -22,22 +22,6 @@ function(test_environment_variables target env_vars)
         string(REGEX REPLACE "=${PROJECT_BINARY_DIR}/;" "=$(OutDir);" MSVC_LOCAL_DEBUGGER_ENVIRONMENT "${MSVC_LOCAL_DEBUGGER_ENVIRONMENT}")
         string(REGEX REPLACE ";" "\n" MSVC_LOCAL_DEBUGGER_ENVIRONMENT "${MSVC_LOCAL_DEBUGGER_ENVIRONMENT}")
 
-        if(MSVC_VERSION GREATER 1800)
-            set(MSVC_TOOLS_VERSION "12.0")
-        elseif(MSVC_VERSION GREATER 1500)
-            set(MSVC_TOOLS_VERSION "4.0")
-        elseif(MSVC_VERSION GREATER 1400)
-            set(MSVC_TOOLS_VERSION "3.5")
-        elseif(MSVC_VERSION GREATER 1301)
-            set(MSVC_TOOLS_VERSION "3.0")
-        elseif(MSVC_VERSION GREATER 1300)
-            set(MSVC_TOOLS_VERSION "1.1")
-        else()
-            set(MSVC_TOOLS_VERSION "1.0")
-        endif()
-
-        set(MSVC_LOCAL_PROPERTY_GROUPS "")
-
         if(CMAKE_SIZEOF_VOID_P EQUAL 8)
             set(MSVC_PLATFORM "x64")
         else()
@@ -45,6 +29,22 @@ function(test_environment_variables target env_vars)
         endif()
 
         if(MSVC_VERSION GREATER 1500)
+
+            if(MSVC_VERSION GREATER 1800)
+                set(MSVC_TOOLS_VERSION "12.0")
+            elseif(MSVC_VERSION GREATER 1500)
+                set(MSVC_TOOLS_VERSION "4.0")
+            elseif(MSVC_VERSION GREATER 1400)
+                set(MSVC_TOOLS_VERSION "3.5")
+            elseif(MSVC_VERSION GREATER 1301)
+                set(MSVC_TOOLS_VERSION "3.0")
+            elseif(MSVC_VERSION GREATER 1300)
+                set(MSVC_TOOLS_VERSION "1.1")
+            else()
+                set(MSVC_TOOLS_VERSION "1.0")
+            endif()
+
+            set(MSVC_LOCAL_PROPERTY_GROUPS "")
 
             set(MSVC_PROPERTY_GROUP_TEMPLATE " \
                 <PropertyGroup Condition=\"'$(Configuration)|$(Platform)'=='@MSVC_BUILD_CONFIG@|@MSVC_PLATFORM@'\">\n \
@@ -55,7 +55,6 @@ function(test_environment_variables target env_vars)
                 </PropertyGroup>\n \
             ")
 
-            set(MSVC_PROJECT_TEMPLATE "${TEST_ENVIRONMENT_VARIABLES_PATH}/ProjectTemplate.vcxproj.user.in")
             set(MSVC_PROJECT_OUTPUT_EXT "vcxproj")
 
             foreach(MSVC_BUILD_CONFIG ${CMAKE_CONFIGURATION_TYPES})
@@ -68,8 +67,55 @@ function(test_environment_variables target env_vars)
 
         else()
 
+            if(MSVC_VERSION EQUAL 2000)
+                set(MSVC_VERSION_NUMBER "15.00")
+            elseif(MSVC_VERSION EQUAL 1900)
+                set(MSVC_VERSION_NUMBER "14.00")
+            elseif(MSVC_VERSION EQUAL 1800)
+                set(MSVC_VERSION_NUMBER "12.00")
+            elseif(MSVC_VERSION EQUAL 1700)
+                set(MSVC_VERSION_NUMBER "11.00")
+            elseif(MSVC_VERSION EQUAL 1600)
+                set(MSVC_VERSION_NUMBER "10.00")
+            elseif(MSVC_VERSION EQUAL 1500)
+                set(MSVC_VERSION_NUMBER "9.00")
+            elseif(MSVC_VERSION EQUAL 1400)
+                set(MSVC_VERSION_NUMBER "8.00")
+            elseif(MSVC_VERSION EQUAL 1310)
+                set(MSVC_VERSION_NUMBER "7.01")
+            elseif(MSVC_VERSION EQUAL 1300)
+                set(MSVC_VERSION_NUMBER "7.00")
+            elseif(MSVC_VERSION EQUAL 1200)
+                set(MSVC_VERSION_NUMBER "6.00")
+            else()
+                set(MSVC_VERSION_NUMBER "5.0")
+            endif()
+
+            set(MSVC_LOCAL_CONFIGURATIONS "")
+
+            set(MSVC_LOCAL_CONFIGURATION_TEMPLATE " \
+                <Configuration Name=\"@MSVC_BUILD_CONFIG@|@MSVC_PLATFORM@\">\n \
+                    <DebugSettings\n \
+                        WorkingDirectory=\"$(OutDir)\"\n \
+                        Environment=\"@MSVC_LOCAL_DEBUGGER_ENVIRONMENT@\"\n \
+                        EnvironmentMerge=\"true\"\n \
+                     /> \
+                </Configuration>\n \
+            ")
+
+            set(MSVC_PROJECT_OUTPUT_EXT "vcproj")
+
+            foreach(MSVC_BUILD_CONFIG ${CMAKE_CONFIGURATION_TYPES})
+
+                string(CONFIGURE ${MSVC_LOCAL_CONFIGURATION_TEMPLATE} MSVC_LOCAL_CONFIGURATION)
+
+                set(MSVC_LOCAL_CONFIGURATIONS "${MSVC_LOCAL_CONFIGURATIONS}${MSVC_LOCAL_CONFIGURATION}\n")
+
+            endforeach()
+
         endif()
 
+        set(MSVC_PROJECT_TEMPLATE "${TEST_ENVIRONMENT_VARIABLES_PATH}/ProjectTemplate.${MSVC_PROJECT_OUTPUT_EXT}.user.in")
         configure_file(${MSVC_PROJECT_TEMPLATE} ${CMAKE_CURRENT_BINARY_DIR}/${target}.${MSVC_PROJECT_OUTPUT_EXT}.user @ONLY)
 
         set(MSVC_LOCAL_CTEST_ENVIRONMENT ${env_vars})
