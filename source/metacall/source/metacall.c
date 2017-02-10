a19e26ce9fe0de6b50f7e5ab7da41d4b0523c42e
Vicente Ferrer <vic798@gmail.com>
2017-02-10 18:30:56 +0100

    Added default logger in debug mode.

--- source/metacall/source/metacall.c
+++ source/metacall/source/metacall.c
@@ -50,13 +50,16 @@ int metacall_initialize()
 	metacall_null_args[0] = NULL;
 
 	/* TODO: Initialize by config or default */
-	/*
-	log_configure("metacall",
-		log_policy_format_text(),
-		log_policy_schedule_sync(),
-		log_policy_storage_sequential(),
-		log_policy_stream_stdio(stdout));
-	*/
+	#if (!defined(NDEBUG) || defined(DEBUG) || defined(_DEBUG) || defined(__DEBUG) || defined(__DEBUG__))
+		if (log_configure("metacall",
+			log_policy_format_text(),
+			log_policy_schedule_sync(),
+			log_policy_storage_sequential(),
+			log_policy_stream_stdio(stdout)) != 0)
+		{
+			return 1;
+		}
+	#endif
 
 	if (configuration_initialize("rapid_json", NULL) != 0)
 	{
