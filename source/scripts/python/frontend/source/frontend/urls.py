f024bb538ba116b15d13d50f6ed303cdb43528df
Vicente Ferrer <vic798@gmail.com>
2017-02-15 08:44:30 +0100

    Added test for django integration (still not working).

--- source/scripts/python/frontend/source/frontend/urls.py
+++ source/scripts/python/frontend/source/frontend/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
