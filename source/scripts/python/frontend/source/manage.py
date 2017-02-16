6cf134271875ae0e7d93176178feec8c7346ce55
Vicente Ferrer <vic798@gmail.com>
2017-02-16 05:54:19 +0100

    Added integration test with django functional, improved django integration with CMake.

--- source/scripts/python/frontend/source/manage.py
+++ source/scripts/python/frontend/source/manage.py
@@ -4,24 +4,24 @@ import os
 import sys
 
 def frontend_execute(args):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frontend.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError:
-        # The above import may fail for some other reason. Ensure that the
-        # issue is really that Django is missing to avoid masking other
-        # exceptions on Python 2.
-        try:
-            import django
-        except ImportError:
-            raise ImportError(
-                "Couldn't import Django. Are you sure it's installed and "
-                "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
-        raise
-    sys.argv = args;
-    execute_from_command_line(sys.argv)
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frontend.settings")
+	try:
+		from django.core.management import execute_from_command_line
+	except ImportError:
+		# The above import may fail for some other reason. Ensure that the
+		# issue is really that Django is missing to avoid masking other
+		# exceptions on Python 2.
+		try:
+			import django
+		except ImportError:
+			raise ImportError(
+				"Couldn't import Django. Are you sure it's installed and "
+				"available on your PYTHONPATH environment variable? Did you "
+				"forget to activate a virtual environment?"
+			)
+		raise
+	sys.argv = args;
+	execute_from_command_line(sys.argv)
 
 def frontend_initialize(port: int) -> int:
 	frontend_execute([os.path.abspath(__file__), 'runserver', str(port)]);
