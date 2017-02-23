98bbf93fce9d6fc8f235441971b09e454e203cc9
Vicente Ferrer <vic798@gmail.com>
2017-02-23 23:27:13 +0100

    Solved error on ruby on rails blog script (still not integrated with tests.

--- source/scripts/ruby/blog/source/blog.rb
+++ source/scripts/ruby/blog/source/blog.rb
@@ -1,5 +1,23 @@
 #!/usr/bin/ruby
 
-ARGV[0] = 'server'
+IO.popen('./bin/rails server') do |io|
+	puts 'Launching rails server on process..', io.pid
 
-require File.expand_path('../bin/rails', __FILE__)
+	Signal.trap('INT') {
+		Process.kill('INT', io.pid)
+
+		io.close
+
+		Signal.trap('INT', 'DEFAULT')
+
+		Process.kill('INT', 0)
+	}
+
+	begin
+		while line = io.gets do
+			puts line
+		end
+	rescue SystemExit, Interrupt
+		exit 0
+	end
+end
