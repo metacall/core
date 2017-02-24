fdb2f3b32980ac18cd75cffb4e27309e9ecfb07a
Vicente Ferrer <vic798@gmail.com>
2017-02-24 05:31:40 +0100

    Added checks for ruby rails integration test.

--- source/tests/rb_rails_integration_test/source/rb_rails_integration_test.cpp
+++ source/tests/rb_rails_integration_test/source/rb_rails_integration_test.cpp
@@ -30,21 +30,21 @@ TEST_F(rb_rails_integration_test, DefaultConstructor)
 			"blog.rb"
 		};
 
-		/*
+		const char func_run[] = "run_and_kill_server";
+
 		void * ret = NULL;
-		*/
- 
+
 		EXPECT_EQ((int) 0, (int) metacall_load_from_file("rb", rb_scripts, sizeof(rb_scripts) / sizeof(rb_scripts[0])));
 
-		/*
-		ret = metacall("run_and_kill_server");
+		EXPECT_NE((void *) NULL, (void *) metacall_function(func_run));
+
+		ret = metacall(func_run);
 
 		EXPECT_NE((void *) NULL, (void *) ret);
 
 		EXPECT_EQ((int) 0, (int) metacall_value_to_int(ret));
 
 		metacall_value_destroy(ret);
-		*/
 	}
 	#endif /* OPTION_BUILD_PLUGINS_RB */
 
