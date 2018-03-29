2ee277bd238b5e7c98a20dc4c304843c1c32d549
Vicente Ferrer <vic798@gmail.com>
2018-03-29 13:50:10 +0200

    Solve minor bug in web script (file was never closed).

--- source/scripts/python/web/source/web.py
+++ source/scripts/python/web/source/web.py
@@ -6,9 +6,8 @@ from datetime import datetime
 def index():
   basepath = path.dirname(path.abspath(__file__))
 
-  f = open(path.join(basepath, 'index.html'), 'r')
-
-  return f.read()
+  with open(path.join(basepath, 'index.html'), 'r') as f:
+    return f.read()
 
 def time():
   return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
