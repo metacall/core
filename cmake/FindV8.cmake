2a86252e2535fc217db12d4fe371f86076bfa646
Vicente Ferrer <vic798@gmail.com>
2018-12-13 03:56:44 +0100

    License all # comment files.

--- cmake/FindV8.cmake
+++ cmake/FindV8.cmake
@@ -1,8 +1,20 @@
 #
 #	CMake Find V8 Google JavaScript Engine by Parra Studios
+#	CMake script to find V8 JavaScript Engine.
+#
 #	Copyright (C) 2016 - 2019 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
 #
-#	CMake script to find V8 JavaScript Engine.
+#	Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
 #
 
 # Find V8 library and include paths
--- tools/metacall-environment.sh
+++ tools/metacall-environment.sh
@@ -2,9 +2,21 @@
 
 #
 #	MetaCall Configuration Environment Bash Script by Parra Studios
+#	Configure and install MetaCall environment script utility.
+#
 #	Copyright (C) 2016 - 2019 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
 #
-#	Configure and install MetaCall environment script utility.
+#	Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
 #
 
 ROOT_DIR=$(pwd)
