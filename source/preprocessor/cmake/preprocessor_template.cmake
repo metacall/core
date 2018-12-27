f98c55a5f879364da439428da1aed355a013144b
viferga <vic798@gmail.com>
2018-12-27 08:04:28 -0900

    Solve minor bugs in preprocessor cmake library.

--- source/preprocessor/cmake/preprocessor_template.cmake
+++ source/preprocessor/cmake/preprocessor_template.cmake
@@ -33,9 +33,9 @@ function(preprocessor_template_generate _headers _body _path _name)
 		set(PREPROCESSOR_TEMPLATE_HEADERS "\n${_headers}")
 	endif()
 
-	set(PREPROCESSOR_TEMPLATE_BODY ${_body})
+	set(PREPROCESSOR_TEMPLATE_BODY "${_body}")
 
-	set(PREPROCESSOR_TEMPLATE_NAME ${_name})
+	set(PREPROCESSOR_TEMPLATE_NAME "${_name}")
 
 	string(TOUPPER "PREPROCESSOR_${_name}_H" PREPROCESSOR_TEMPLATE_NAME_HEADER)
 
@@ -43,17 +43,17 @@ function(preprocessor_template_generate _headers _body _path _name)
 	set(preprocessor_output_file "${_path}/preprocessor_${_name}.h")
 
 	# Read template
-	file(READ ${preprocessor_path}/preprocessor_template.h.in preprocessor_template_in)
+	file(READ "${preprocessor_path}/preprocessor_template.h.in" preprocessor_template_in)
 
 	# Configure template string
-	string(CONFIGURE ${preprocessor_template_in} preprocessor_output_file_str)
+	string(CONFIGURE "${preprocessor_template_in}" preprocessor_output_file_str)
 
 	# Check against differences
-	file(READ ${preprocessor_output_file} preprocessor_prev_output_file_str)
+	file(READ "${preprocessor_output_file}" preprocessor_prev_output_file_str)
 
 	# Write output file if there are differences
-	if(NOT ${preprocessor_output_file_str} STREQUAL ${preprocessor_prev_output_file_str})
-		file(WRITE ${preprocessor_output_file} ${preprocessor_output_file_str})
+	if(NOT "${preprocessor_output_file_str}" STREQUAL "${preprocessor_prev_output_file_str}")
+		file(WRITE "${preprocessor_output_file}" "${preprocessor_output_file_str}")
 	endif()
 
 endfunction()
