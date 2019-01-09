bcaea1b6896229c3995e33da2754d57b6ff09a1e
viferga <vic798@gmail.com>
2019-01-08 15:56:49 -0900

    Add first implementation for python call benchmark with fixture.

--- source/benchmarks/metacall_py_call_bench/source/metacall_py_call_bench.cpp
+++ source/benchmarks/metacall_py_call_bench/source/metacall_py_call_bench.cpp
@@ -21,32 +21,108 @@
 #include <benchmark/benchmark.h>
 
 #include <metacall/metacall.h>
+#include <metacall/metacall_loaders.h>
 
-class python_call_bench : public benchmark::Fixture
+class metacall_py_call_bench : public benchmark::Fixture
 {
 public:
+	void SetUp(benchmark::State & state)
+	{
+		metacall_log_stdio_type log_stdio = { stdout };
+
+		metacall_print_info();
+
+		if (metacall_log(METACALL_LOG_STDIO, static_cast<void *>(&log_stdio)) != 0)
+		{
+			state.SkipWithError("Error initializing MetaCall Log");
+		}
+
+		if (metacall_initialize() != 0)
+		{
+			state.SkipWithError("Error initializing MetaCall");
+		}
+
+		/* Python */
+		#if defined(OPTION_BUILD_LOADERS_PY)
+		{
+			static const char tag[] = "py";
+
+			static const char int_mem_type[] =
+				"#!/usr/bin/python3.5\n"
+				"def int_mem_type(left: int, right: int) -> int:\n"
+				"\treturn 0;";
+
+			if (metacall_load_from_memory(tag, int_mem_type, sizeof(int_mem_type), NULL) != 0)
+			{
+				state.SkipWithError("Error loading int_mem_type function");
+			}
+		}
+		#endif /* OPTION_BUILD_LOADERS_PY */
+	}
+
+	void TearDown(benchmark::State & state)
+	{
+		if (metacall_destroy() != 0)
+		{
+			state.SkipWithError("Error destroying MetaCall");
+		}
+	}
 };
 
-BENCHMARK_F(python_call_bench, call_va_args)(benchmark::State & st)
+BENCHMARK_DEFINE_F(metacall_py_call_bench, va_args_call)(benchmark::State & state)
 {
-	metacall_print_info();
+	/*
+	const int64_t call_count = 10000;
+	const int64_t call_size = sizeof(long) * 3; // (long, long) -> long
+	*/
 
-	metacall_log_stdio_type log_stdio = { stdout };
-
-	#if 0
-	ASSERT_EQ((int) 0, (int) metacall_log(METACALL_LOG_STDIO, (void *)&log_stdio));
-
-	ASSERT_EQ((int) 0, (int) metacall_initialize());
-
-	/* Python */
-	#if defined(OPTION_BUILD_LOADERS_PY)
+	for (auto _ : state)
 	{
-		/* TODO */
-	}
-	#endif /* OPTION_BUILD_LOADERS_PY */
+		/* Python */
+		#if defined(OPTION_BUILD_LOADERS_PY)
+		{
+			void * ret;
 
-	EXPECT_EQ((int) 0, (int) metacall_destroy());
-	#endif
+			/*
+			benchmark::DoNotOptimize(ret = metacall("int_mem_type", 0L, 0L));
+			*/
+
+			ret = metacall("int_mem_type", 0L, 0L);
+
+			/*
+			state.PauseTiming();
+
+			if (ret == NULL)
+			{
+				state.SkipWithError("Null return value from int_mem_type");
+			}
+
+			if (metacall_value_to_long(ret) != 0L)
+			{
+				state.SkipWithError("Invalid return value from int_mem_type");
+			}
+
+			metacall_value_destroy(ret);
+
+			state.ResumeTiming();
+			*/
+		}
+		#endif /* OPTION_BUILD_LOADERS_PY */
+	}
+
+	/*
+	state.SetLabel("MetaCall Python Call Benchmark - Variadic Argument Call");
+	state.SetBytesProcessed(call_size * call_count);
+	state.SetItemsProcessed(call_count);
+	*/
 }
+
+BENCHMARK_REGISTER_F(metacall_py_call_bench, va_args_call)
+	->Threads(1)
+	->Unit(benchmark::kNanosecond)
+	/*->Iterations(10000)*/
+	->Iterations(1)
+	/*->Repetitions(10)*/;
+	->Repetitions(1);
 
 BENCHMARK_MAIN();
