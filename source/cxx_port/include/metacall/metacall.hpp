14b2ed11e7724f2d7dde29b53f5c64507cfbed83
viferga <vic798@gmail.com>
2019-01-11 01:12:35 -0900

    Add base for cxx port working properly.

---
+++ source/cxx_port/include/metacall/metacall.hpp
@@ -0,0 +1,40 @@
+/*
+ *	Format Library by Parra Studios
+ *	A cross-platform library for supporting formatted input / output.
+ *
+ *	Copyright (C) 2016 - 2019 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ *
+ */
+
+#ifndef METACALL_HPP
+#define METACALL_HPP 1
+
+/* -- Headers -- */
+
+#include <metacall/metacall_api.hpp>
+
+#include <string>
+
+namespace metacall
+{
+
+template <typename... Ts>
+METACALL_API int metacall(std::string name, Ts... ts);
+
+} /* namespace metacall */
+
+#include <metacall/metacall.inl>
+
+#endif /* METACALL_HPP */
