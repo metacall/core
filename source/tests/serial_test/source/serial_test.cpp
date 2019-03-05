ac0ba897d9d80c53012ade24cfea472bd0756011
viferga <vic798@gmail.com>
2019-03-05 12:13:46 -0900

    Solve serial test for apple.

--- source/tests/serial_test/source/serial_test.cpp
+++ source/tests/serial_test/source/serial_test.cpp
@@ -301,7 +301,7 @@ TEST_F(serial_test, DefaultConstructor)
 				#else
 					"0x000A7EF2",
 				#endif
-			#elif defined(__linux) || defined(__linux__)
+			#elif defined(__linux) || defined(__linux__) || defined(__APPLE__)
 				"0xa7ef2",
 			#else
 				"<unknown>",
