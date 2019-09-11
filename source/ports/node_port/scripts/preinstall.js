5f1affb555f9846a3af2509e4f5d8568dddc1ec3
viferga <vic798@gmail.com>
2019-09-11 14:33:23 -0800

    Commented node port preinstall by now.

--- source/ports/node_port/scripts/preinstall.js
+++ source/ports/node_port/scripts/preinstall.js
@@ -1,18 +1,15 @@
 'use strict';
 
-const path = require('path');
-const fs = require("fs");
+/* TODO: Detect if metacall is already installed in the system and skip the this step */
 
-/* TODO */
+/* TODO: Detect platform and architecture and download the binaries */
 
+/*
 switch (process.platform)
 {
 	case "linux":
-		if (!fs.existsSync("/usr/lib/libmetacall.so"))
-		{
-			console.log("I think you haven't installed metacall, coz i can't find it in /usr/lib/")
-		}
 		break;
 	case "win32":
-		if (!process.env.METACALL_HOME) console.log("try install metacall with the windows installer")
+		break;
 }
+*/
