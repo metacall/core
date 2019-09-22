2eaa7e62db5d0d61fefc9f6b37316e2786518530
zedonboy <zedonbiz@gmail.com>
2019-09-22 20:44:47 +0100

    Still working on Discover function

--- source/loaders/mock_loader/source/mock_loader_impl.c
+++ source/loaders/mock_loader/source/mock_loader_impl.c
@@ -33,21 +33,20 @@
 
 typedef struct loader_impl_mock_type
 {
-	void * impl_mock_data;
+	void *impl_mock_data;
 
 } * loader_impl_mock;
 
 typedef struct loader_impl_mock_handle_type
 {
-	void * handle_mock_data;
+	void *handle_mock_data;
 
 } * loader_impl_mock_handle;
-
 
 typedef struct loader_impl_mock_function_type
 {
 	loader_impl_mock_handle mock_handle;
-	void * function_mock_data;
+	void *function_mock_data;
 
 } * loader_impl_mock_function;
 
@@ -65,7 +64,7 @@ function_return function_mock_interface_invoke(function func, function_impl impl
 
 	signature s = function_signature(func);
 
-	const char * name = function_name(func);
+	const char *name = function_name(func);
 
 	type ret_type = signature_get_return(s);
 
@@ -89,55 +88,55 @@ function_return function_mock_interface_invoke(function func, function_impl impl
 
 			if (id == TYPE_BOOL)
 			{
-				boolean * value_ptr = (boolean *)(args[args_count]);
+				boolean *value_ptr = (boolean *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Boolean value: %d", *value_ptr);
 			}
 			else if (id == TYPE_CHAR)
 			{
-				char * value_ptr = (char *)(args[args_count]);
+				char *value_ptr = (char *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Char value: %c", *value_ptr);
 			}
 			else if (id == TYPE_SHORT)
 			{
-				short * value_ptr = (short *)(args[args_count]);
+				short *value_ptr = (short *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Short value: %d", *value_ptr);
 			}
 			else if (id == TYPE_INT)
 			{
-				int * value_ptr = (int *)(args[args_count]);
+				int *value_ptr = (int *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Int value: %d", *value_ptr);
 			}
 			else if (id == TYPE_LONG)
 			{
-				long * value_ptr = (long *)(args[args_count]);
+				long *value_ptr = (long *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Long value: %ld", *value_ptr);
 			}
 			else if (id == TYPE_FLOAT)
 			{
-				float * value_ptr = (float *)(args[args_count]);
+				float *value_ptr = (float *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Float value: %f", *value_ptr);
 			}
 			else if (id == TYPE_DOUBLE)
 			{
-				double * value_ptr = (double *)(args[args_count]);
+				double *value_ptr = (double *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Double value: %f", *value_ptr);
 			}
 			else if (id == TYPE_STRING)
 			{
-				const char * value_ptr = (const char *)(args[args_count]);
+				const char *value_ptr = (const char *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "String value: %s", value_ptr);
 			}
 			else if (id == TYPE_PTR)
 			{
-				void * value_ptr = (void *)(args[args_count]);
+				void *value_ptr = (void *)(args[args_count]);
 
 				log_write("metacall", LOG_LEVEL_DEBUG, "Pointer value: %p", value_ptr);
 			}
@@ -211,7 +210,7 @@ function_return function_mock_interface_invoke(function func, function_impl impl
 	return NULL;
 }
 
-function_return function_mock_interface_await(function func, function_impl impl, function_args args, function_resolve_callback resolve_callback, function_reject_callback reject_callback, void * context)
+function_return function_mock_interface_await(function func, function_impl impl, function_args args, function_resolve_callback resolve_callback, function_reject_callback reject_callback, void *context)
 {
 	/* TODO */
 
@@ -240,12 +239,11 @@ void function_mock_interface_destroy(function func, function_impl impl)
 function_interface function_mock_singleton(void)
 {
 	static struct function_interface_type mock_interface =
-	{
-		&function_mock_interface_create,
-		&function_mock_interface_invoke,
-		&function_mock_interface_await,
-		&function_mock_interface_destroy
-	};
+		{
+			&function_mock_interface_create,
+			&function_mock_interface_invoke,
+			&function_mock_interface_await,
+			&function_mock_interface_destroy};
 
 	return &mock_interface;
 }
@@ -257,21 +255,19 @@ int mock_loader_impl_initialize_types(loader_impl impl)
 	static struct
 	{
 		type_id id;
-		const char * name;
-	}
-	type_id_name_pair[] =
-	{
-		{ TYPE_BOOL,	"Boolean"	},
-		{ TYPE_CHAR,	"Char"		},
-		{ TYPE_SHORT,	"Short"		},
-		{ TYPE_INT,		"Integer"	},
-		{ TYPE_LONG,	"Long"		},
-		{ TYPE_DOUBLE,	"Float"		},
-		{ TYPE_DOUBLE,	"Double"	},
-		{ TYPE_STRING,	"String"	},
-		{ TYPE_BUFFER,	"Buffer"	},
-		{ TYPE_PTR,		"Ptr"		}
-	};
+		const char *name;
+	} type_id_name_pair[] =
+		{
+			{TYPE_BOOL, "Boolean"},
+			{TYPE_CHAR, "Char"},
+			{TYPE_SHORT, "Short"},
+			{TYPE_INT, "Integer"},
+			{TYPE_LONG, "Long"},
+			{TYPE_DOUBLE, "Float"},
+			{TYPE_DOUBLE, "Double"},
+			{TYPE_STRING, "String"},
+			{TYPE_BUFFER, "Buffer"},
+			{TYPE_PTR, "Ptr"}};
 
 	size_t index, size = sizeof(type_id_name_pair) / sizeof(type_id_name_pair[0]);
 
@@ -352,7 +348,7 @@ loader_handle mock_loader_impl_load_from_file(loader_impl impl, const loader_nam
 	return NULL;
 }
 
-loader_handle mock_loader_impl_load_from_memory(loader_impl impl, const loader_naming_name name, const char * buffer, size_t size)
+loader_handle mock_loader_impl_load_from_memory(loader_impl impl, const loader_naming_name name, const char *buffer, size_t size)
 {
 	loader_impl_mock_handle handle = malloc(sizeof(struct loader_impl_mock_handle_type));
 
--- source/loaders/rpc_loader/source/rpc_loader_impl.c
+++ source/loaders/rpc_loader/source/rpc_loader_impl.c
@@ -26,30 +26,74 @@
 #include <reflect/reflect_function.h>
 #include <reflect/reflect_scope.h>
 #include <reflect/reflect_context.h>
+#include <serial/serial.h>
 
 #include <log/log.h>
 
+#define METACALL "metacall.txt"
+#define BUFFER_SIZE 256
+#define RAPID_JSON "rapid_json"
+
 // The Curl Libraries
 #include "curl/curl.h"
+#include "stdio.h"
+#include "ctype.h"
 #include <stdlib.h>
+#include "string.h"
 
 typedef struct loader_impl_rpc_function_type
 {
-	void * todo;
+	loader_impl_rpc_handle *handle;
+	void *func_rpc_data;
 
 } * loader_impl_rpc_function;
 
 typedef struct loader_impl_rpc_handle_type
 {
-	void * todo;
+	void *todo;
 
 } * loader_impl_rpc_handle;
 
 typedef struct loader_impl_rpc_type
 {
-	CURL * curl;
+	CURL *curl;
 
 } * loader_impl_rpc;
+
+typedef struct JsonData
+{
+	size_t string_length;
+	char *data;
+	size_t buffer_size;
+} JsonData;
+
+void jsondata_constructor(JsonData *this)
+{
+	this->data = malloc(BUFFER_SIZE);
+	this->data[0] = "\0"; // trying to make it an empty null terminated string
+	this->string_length = 1;
+	this->buffer_size = BUFFER_SIZE;
+}
+
+void jsondata_append(JsonData *this, char c)
+{
+	if (this->string_length < this->buffer_size)
+	{
+		this->data[this->string_length - 1] = c;
+		this->data[this->string_length] = "\0";
+		this->string_length++;
+	}
+	else
+	{
+		this->data = realloc(this->data, this->buffer_size + BUFFER_SIZE);
+		this->buffer_size = this->buffer_size + BUFFER_SIZE;
+	}
+}
+
+void jsondata_destructor(JsonData *this)
+{
+	free(this->data);
+}
 
 int type_rpc_interface_create(type t, type_impl impl)
 {
@@ -72,10 +116,9 @@ void type_rpc_interface_destroy(type t, type_impl impl)
 type_interface type_rpc_singleton(void)
 {
 	static struct type_interface_type rpc_type_interface =
-	{
-		&type_rpc_interface_create,
-		&type_rpc_interface_destroy
-	};
+		{
+			&type_rpc_interface_create,
+			&type_rpc_interface_destroy};
 
 	return &rpc_type_interface;
 }
@@ -101,7 +144,7 @@ function_return function_rpc_interface_invoke(function func, function_impl impl,
 	return NULL;
 }
 
-function_return function_rpc_interface_await(function func, function_impl impl, function_args args, function_resolve_callback resolve_callback, function_reject_callback reject_callback, void * context)
+function_return function_rpc_interface_await(function func, function_impl impl, function_args args, function_resolve_callback resolve_callback, function_reject_callback reject_callback, void *context)
 {
 	/* TODO */
 
@@ -126,12 +169,11 @@ void function_rpc_interface_destroy(function func, function_impl impl)
 function_interface function_rpc_singleton(void)
 {
 	static struct function_interface_type rpc_function_interface =
-	{
-		&function_rpc_interface_create,
-		&function_rpc_interface_invoke,
-		&function_rpc_interface_await,
-		&function_rpc_interface_destroy
-	};
+		{
+			&function_rpc_interface_create,
+			&function_rpc_interface_invoke,
+			&function_rpc_interface_await,
+			&function_rpc_interface_destroy};
 
 	return &rpc_function_interface;
 }
@@ -149,6 +191,8 @@ loader_impl_data rpc_loader_impl_initialize(loader_impl impl, configuration conf
 	}
 
 	log_copy(host->log);
+
+	curl_global_init(CURL_GLOBAL_ALL);
 
 	rpc_impl->curl = curl_easy_init();
 
@@ -183,7 +227,7 @@ loader_handle rpc_loader_impl_load_from_file(loader_impl impl, const loader_nami
 	return (loader_handle)NULL;
 }
 
-loader_handle rpc_loader_impl_load_from_memory(loader_impl impl, const loader_naming_name name, const char * buffer, size_t size)
+loader_handle rpc_loader_impl_load_from_memory(loader_impl impl, const loader_naming_name name, const char *buffer, size_t size)
 {
 	/* TODO */
 
@@ -215,15 +259,103 @@ int rpc_loader_impl_clear(loader_impl impl, loader_handle handle)
 	return 0;
 }
 
+static loader_impl_rpc_function rpc_function_create(loader_impl_rpc_handle handle)
+{
+	loader_impl_rpc_function rpc_func = malloc(sizeof(struct loader_impl_rpc_function_type));
+	if (rpc_func != NULL)
+	{
+		rpc_func->handle = handle;
+		rpc_func->func_rpc_data = NULL;
+	}
+
+	return rpc_func;
+}
+
+static size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp)
+{
+}
+
 int rpc_loader_impl_discover(loader_impl impl, loader_handle handle, context ctx)
 {
-	/* TODO */
+	FILE *fp = fopen(METACALL, "r");
+	if (fp == NULL)
+	{
+		log_write("metacall", LOG_LEVEL_ERROR, "Could not find/open api endpoint descriptor file");
+		return 1;
+	}
+	size_t idx = 0, array_idx = 0;
+	char currChar;
+	char **array_of_strings = malloc(BUFFER_SIZE);
+	while ((currChar = fgetc(fp)) != EOF)
+	{
+		if (idx == BUFFER_SIZE || array_idx == BUFFER_SIZE)
+		{
+			log_write("metacall", LOG_LEVEL_ERROR, "each endpoint is larger than the buffer size");
+			return 1;
+		}
+		if (isblank(currChar))
+			continue;
+		char *endpoint = malloc(BUFFER_SIZE); // i believe malloc will always be successfull :).
+		if (currChar != "\n")
+		{
+			endpoint[idx++] = currChar;
+		}
+		else
+		{
+			endpoint[idx] = "\0"; // making it a null_terminated string to make it manipulatable by string.h
+			idx = 0;			  // resset the counter.
+			array_of_strings[array_idx++] = endpoint;
+		}
+	}
 
-	(void)impl;
-	(void)handle;
-	(void)ctx;
+	// making http request
+	loader_impl_rpc rpc = loader_impl_get(impl);
+	curl_easy_setopt(rpc->curl, CURLOPT_POSTFIELDS, "");
+	curl_easy_setopt(rpc->curl, CURLOPT_WRITEFUNCTION, write_data);
+	memory_allocator allocator = memory_allocator_std(&malloc, &realloc, &free);
+	serial s = serial_create(RAPID_JSON);
+	for (size_t i = 0; i < array_idx; i++)
+	{
 
-	return 0;
+		strcat(array_of_strings[i], "/inspect");
+		curl_easy_setopt(rpc->curl, CURLOPT_URL, array_of_strings[i]);
+		JsonData jd;
+		jsondata_constructor(&jd);
+		curl_easy_setopt(rpc->curl, CURLOPT_WRITEDATA, jd);
+		CURLcode res = curl_easy_perform(rpc->curl);
+		if (res == CURLE_OK)
+		{
+			
+			value map = serial_deserialize(s, jd.data, jd.string_length, allocator);
+			value arrayValue = get_funcs(map);
+			value *array_of_maps = value_to_array(arrayValue);
+			size_t iterator, array_size = value_type_count(arrayValue);
+			scope sp = context_scope(ctx);
+			for (size_t i = 0; i < array_size; i++)
+			{
+				loader_impl_rpc_function rpc_func = rpc_function_create((loader_impl_rpc_handle)handle);
+				value kmap = array_of_maps[i];
+				value *array_of_array = value_to_map(kmap);
+				const char *funcName = value_to_string(array_of_array[0][1]);
+				kmap = array_of_maps[1][1];
+				array_of_array = value_to_map(kmap);
+				const char *funcRet = value_to_string(array_of_array[0][1]);
+				arrayValue = array_of_array[1][1];
+				size_t argc = value_type_count(arrayValue);
+				function f = function_create(funcName, argc, rpc_func, &function_rpc_singleton);
+				signature s = function_signature(f);
+				signature_set_return(s, loader_impl_type(impl, funcRet));
+				
+			}
+		}
+		else
+		{
+			log_write("metacall", LOG_LEVEL_ERROR, "Could not reach Metacall servers");
+			return 1;
+		}
+	}
+
+	// Free all Prisoners(pointers)
 }
 
 int rpc_loader_impl_destroy(loader_impl impl)
@@ -232,5 +364,19 @@ int rpc_loader_impl_destroy(loader_impl impl)
 
 	curl_easy_cleanup(rpc_impl->curl);
 
+	curl_global_cleanup();
+
 	return 0;
+}
+
+static value get_funcs(value map)
+{
+	value *map_value = value_to_map(map);
+	value *pair = value_to_array(map_value[0]);
+	pair = value_to_array(pair[1]); // trying to reuse the variable thats all
+	value *map1 = value_to_map(pair[0]);
+	pair = value_to_array(map1[1]);
+	map1 = value_to_map(pair[1]);
+	pair = value_to_array(map1[1]);
+	return pair[1]
 }
