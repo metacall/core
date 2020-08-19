7bfacba4baa0d6cf34b915cfea3d65e5c53c9304
viferga <vic798@gmail.com>
2020-08-19 15:37:13 -0800

    Remove test from go port.

--- source/ports/go_port/source/go_port.go
+++ source/ports/go_port/source/go_port.go
@@ -111,3 +111,33 @@ func metacall(function string, args ...interface{}) (interface{}, error) {
 func metacall_destroy() {
 	C.metacall_destroy()
 }
+
+/*
+func main() {
+
+	if err := metacall_initialize(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	defer metacall_destroy()
+
+	scripts := []string{ "test.mock" }
+
+	if err := metacall_load_from_file("mock", scripts); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ret, err := metacall("three_str", "e", "f", "g")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if str, ok := ret.(string); ok {
+		fmt.Println(str)
+	}
+}
+*/
