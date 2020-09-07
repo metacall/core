b407f32fbe4328dedcad8833500a3c589337321d
zedonboy <zedonbiz@gmail.com>
2020-09-07 22:34:11 +0100

    Completed python Monkey patching NOT Tested yet

--- source/ports/py_port/package/metacall/__init__.py
+++ source/ports/py_port/package/metacall/__init__.py
@@ -20,33 +20,110 @@
 import os
 import sys
 import re
+import json
+import types
 
+# Am trying to mimick js index.js
 # Append environment variable or default install path when building manually (TODO: Cross-platform paths)
-sys.path.append(os.environ.get('PORT_LIBRARY_PATH', os.path.join(os.path.sep, 'usr', 'local', 'lib')));
+sys.path.append(os.environ.get('PORT_LIBRARY_PATH',
+                               os.path.join(os.path.sep, 'usr', 'local', 'lib')))
 
 # Find is MetaCall is installed as a distributable tarball (TODO: Cross-platform paths)
 rootdir = os.path.join(os.path.sep, 'gnu', 'store')
 regex = re.compile('.*-metacall-.*')
 
 for root, dirs, _ in os.walk(rootdir):
-	for folder in dirs:
-		if regex.match(folder) and not folder.endswith('R'):
-			sys.path.append(os.path.join(rootdir, folder, 'lib'))
+    for folder in dirs:
+        if regex.match(folder) and not folder.endswith('R'):
+            sys.path.append(os.path.join(rootdir, folder, 'lib'))
 
 try:
-	from _py_port import * # TODO: Import only the functions that will be exported
+    # TODO: Import only the functions that will be exported
+    from _py_port import metacall_inspect, metacall_load_from_file, metacall
 except ImportError as e:
-	try:
-		print('Error when importing MetaCall Python Port:', e);
-		from _py_portd import * # TODO: Import only the functions that will be exported
-		print('MetaCall Python Port Debug Imported');
-	except ImportError as e:
-		print('\x1b[31m\x1b[1m', 'You do not have MetaCall installed or we cannot find it (', e, ')\x1b[0m');
-		print('\x1b[33m\x1b[1m', 'If you do not have it installed, you have three options:', '\x1b[0m');
-		print('\x1b[1m', '	1) Go to https://github.com/metacall/install and install it.', '\x1b[0m');
-		print('\x1b[1m', '	2) Contribute to https://github.com/metacall/distributable by providing support for your platform and architecture.', '\033[0m');
-		print('\x1b[1m', '	3) Be a x10 programmer and compile it by yourself, then define the install folder (if it is different from the default /usr/local/lib) in os.environ[\'LOADER_LIBRARY_PATH\'].', '\x1b[0m');
-		print('\x1b[33m\x1b[1m', 'If you have it installed in an non-standard folder, please define os.environ[\'LOADER_LIBRARY_PATH\'].', '\x1b[0m');
-		pass
+    try:
+        print('Error when importing MetaCall Python Port:', e)
+        from _py_portd import *  # TODO: Import only the functions that will be exported
+        print('MetaCall Python Port Debug Imported')
+    except ImportError as e:
+        print('\x1b[31m\x1b[1m',
+              'You do not have MetaCall installed or we cannot find it (', e, ')\x1b[0m')
+        print('\x1b[33m\x1b[1m',
+              'If you do not have it installed, you have three options:', '\x1b[0m')
+        print(
+            '\x1b[1m', '	1) Go to https://github.com/metacall/install and install it.', '\x1b[0m')
+        print('\x1b[1m', '	2) Contribute to https://github.com/metacall/distributable by providing support for your platform and architecture.', '\033[0m')
+        print('\x1b[1m', '	3) Be a x10 programmer and compile it by yourself, then define the install folder (if it is different from the default /usr/local/lib) in os.environ[\'LOADER_LIBRARY_PATH\'].', '\x1b[0m')
+        print('\x1b[33m\x1b[1m', 'If you have it installed in an non-standard folder, please define os.environ[\'LOADER_LIBRARY_PATH\'].', '\x1b[0m')
+        pass
 
 # TODO: Monkey patch
+
+
+def inspect():
+    jsonData = metacall_inspect()
+    if jsonData:
+        dic = json.loads(jsonData)
+        try:
+            del dic['__metacall_host__']
+            return dic
+        except KeyError as e:
+            print('\x1b[31m\x1b[1m',
+                  'Could not delete __metacall_host__ key (', e, ')\x1b[0m')
+            pass
+        pass
+    return dict()
+
+
+def setUpModule(nameMod):
+    return sys.modules.setdefault(nameMod, types.ModuleType(nameMod))
+
+
+def metacall_require(tag, namePath, id):
+    metacall_load_from_file(tag, [namePath])
+    inspectData = inspect()
+    if not inspectData:
+            print('\x1b[31m\x1b[1m',
+                  'Inspect Data from metacall_inspect is empty\x1b[0m')
+            return
+    listOfScripts = inspectData[tag]
+    script = next((x for x in listOfScripts if x.name == id), None)
+    mod = setUpModule(id)
+    for func in script['scope']['funcs']:
+        mod.__dict__[func.name] = lambda *args: metacall(func.name, *args)
+        pass
+    pass
+
+
+# Get Files from LOADER_SCRIPTS_PATH
+scriptsFolder = None
+try:
+    scriptsFolder = os.environ['LOADER_SCRIPT_PATH']
+except KeyError as e:
+    print('\x1b[31m\x1b[1m',
+          'There is no environment variable called LOADER_SCRIPT_PATH (', e, ')\x1b[0m')
+    pass
+if not os.path.isdir(scriptsFolder):
+    print('\x1b[31m\x1b[1m',
+          'LOADER_SCRIPTS_PATH should be a folder/directory (', e, ')\x1b[0m')
+    pass
+
+tags = {
+    ['mock']: 'mock',
+    ['js', 'node']: 'node',
+    ['py']: 'py',
+    ['rb']: 'rb',
+    ['vb', 'cs']: 'cs'
+}
+
+files = os.listdir(scriptsFolder)
+for script in files:
+    scriptAbsPAth = os.path.join(scriptsFolder, script)
+    if os.path.isfile(scriptAbsPAth):
+        fileNameAndExtensionList = script.split('.')
+        extension = fileNameAndExtensionList[1]
+        mtag = tags[extension]
+        metacall_require(
+            mtag, scriptAbsPAth, fileNameAndExtensionList[0])
+        pass
+    pass
