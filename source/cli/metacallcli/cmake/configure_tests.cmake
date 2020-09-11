3c288c4bb820a39730e31befb69e456def7ecdbc
Vicente Ferrer <vic798@gmail.com>
2020-09-11 02:32:50 +0200

    Solve minor bugs in metacallcli tests.

--- source/cli/metacallcli/cmake/configure_tests.cmake
+++ source/cli/metacallcli/cmake/configure_tests.cmake
@@ -1,5 +1,3 @@
 # Configure Python Port path
-set(PROJECT_METACALL_PORTS_DIRECTORY "${CMAKE_SOURCE_DIR}/source/ports/py_port")
-
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/test/cli-test.py.in" "${LOADER_SCRIPT_PATH}/cli-test.py" @ONLY)
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/test/cli-test-rb.py.in" "${LOADER_SCRIPT_PATH}/cli-test-rb.py" @ONLY)
