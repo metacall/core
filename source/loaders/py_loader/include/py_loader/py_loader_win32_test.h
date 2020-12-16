8236de446f415cf285781272720b28dbbd5f02ab
Vicente Ferrer <vic798@gmail.com>
2020-12-16 20:29:31 +0100

    Add base for test loading symbols from process in windows.

---
+++ source/loaders/py_loader/include/py_loader/py_loader_win32_test.h
@@ -0,0 +1,34 @@
+/*
+ *	Loader Library by Parra Studios
+ *	A plugin for loading python code at run-time into a process.
+ *
+ *	Copyright (C) 2016 - 2020 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ *
+ */
+
+#ifndef PY_LOADER_WIN32_TEST_H
+#define PY_LOADER_WIN32_TEST_H 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int PrintModules();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PY_LOADER_PORT_H */
