b51c4d39663ddfbbf94c9e7d51e69c29d2e5815d
Tabzz98 <muhammadtabaza@gmail.com>
2021-01-15 20:47:30 +0200

    Moved test code to MetaCallSpec.scala

--- source/ports/scala_port/src/main/scala/MetaCall.scala
+++ source/ports/scala_port/src/main/scala/MetaCall.scala
@@ -21,7 +21,6 @@
 package metacall
 
 import com.sun.jna._
-import java.nio.file.Paths
 
 class SizeT(value: Long) extends IntegerType(Native.SIZE_T_SIZE, value) {
   def this() = this(0)
@@ -97,86 +96,6 @@ trait MetaCallBindings extends Library {
   // TODO:
   def metacall_value_id(v: Pointer): Int /* enum metacall_value_id */
 }
-
-object MetaCall {
-  var metacall = Native.load("metacall", classOf[MetaCallBindings]);
-
-  // This is a experiment I have been doing in order to implement a high level
-  // abstraction for Scala. The objective is to make it as trasparent as possible,
-  // like if you were dealing with normal scala values. Generics can help for sure.
-
-  /*
-  trait ValueLifetime {
-    def finalize()
-  }
-
-  def finalizer [T, V <: ValueLifetime] (v : V) (f : V => T) : T =
-    try f(v)
-    finally v.finalize()
-
-  abstract class ValuePtr[T](v: Pointer) extends ValueLifetime {
-    def finalize() {
-      metacall.metacall_value_destroy(v)
-    }
-  }
-
-  class Value[@specialized(Int) T](i: Int) extends ValuePtr[T](metacall.metacall_value_create_int(i)) {
-    def to_value(): T = {
-      metacall.metacall_value_to_int(v)
-    }
-  }
-
-  class Value[@specialized(String) T](str: String) extends ValuePtr(metacall.metacall_value_create_string(i)) {
-
-  }
-   */
-
-  if (metacall.metacall_initialize() != 0) {
-    throw new RuntimeException("MetaCall could not initialize")
-  }
-
-  var paths = Array(
-    Paths.get("./src/test/scala/scripts/main.py").toAbsolutePath.toString()
-  )
-
-  // Load the script list
-  if (
-    metacall.metacall_load_from_file(
-      "py",
-      paths,
-      new SizeT(paths.length.toLong),
-      null
-    ) != 0
-  ) {
-    throw new RuntimeException("MetaCall failed to load the script")
-  }
-
-  // Create array of parameters
-  var args = Array(
-    metacall.metacall_value_create_int(3),
-    metacall.metacall_value_create_int(5)
-  )
-
-  // Invoke the function
-  var ret = metacall.metacallv_s(
-    "hello_sacala_from_python",
-    args,
-    new SizeT(args.length.toLong)
-  )
-
-  // Note: Python uses longs, so it returns a long value
-  println("Result:" + metacall.metacall_value_to_long(ret))
-
-  // For avoiding conversion errors, it is possible to test against metacall_value_id,
-  // or there is also a casting API for dealing with it.
-
-  // Clear parameters
-  args.foreach { metacall.metacall_value_destroy }
-
-  // Clear return value
-  metacall.metacall_value_destroy(ret)
-
-  if (metacall.metacall_destroy() != 0) {
-    throw new RuntimeException("MetaCall did not finish successfully")
-  }
+object MetaCallBindings {
+  val instance = Native.load("metacall", classOf[MetaCallBindings])
 }
--- source/ports/scala_port/src/test/scala/MetaCallSpec.scala
+++ source/ports/scala_port/src/test/scala/MetaCallSpec.scala
@@ -1,9 +1,97 @@
 package metacall
 
+import java.nio.file.Paths
 import org.scalatest.flatspec.AnyFlatSpec
 
 class MetaCallSpec extends AnyFlatSpec {
-  "MetaCall" should "work" in {
-    MetaCall
+  val metacall = MetaCallBindings.instance
+
+  val scriptPaths = Array(
+    Paths.get("./src/test/scala/scripts/main.py").toAbsolutePath.toString()
+  )
+
+  "MetaCall" should "initialize correctly" in {
+    require(
+      metacall.metacall_initialize() == 0,
+      "MetaCall could not initialize"
+    )
+  }
+
+  "MetaCall" should "load script successsfully" in {
+    require(
+      metacall.metacall_load_from_file(
+        "py",
+        scriptPaths,
+        new SizeT(scriptPaths.length.toLong),
+        null
+      ) == 0,
+      "MetaCall failed to load the script"
+    )
+  }
+
+  "MetaCall" should "successfully call function from loaded script and return correct value" in {
+    // Create array of parameters
+    val args = Array(
+      metacall.metacall_value_create_int(3),
+      metacall.metacall_value_create_int(5)
+    )
+
+    // Invoke the function
+    val retPtr = metacall.metacallv_s(
+      "hello_sacala_from_python",
+      args,
+      new SizeT(args.length.toLong)
+    )
+
+    // Note: Python uses longs, so it returns a long value
+    val retValue = metacall.metacall_value_to_long(retPtr)
+
+    assert(retValue == 8)
+
+    // For avoiding conversion errors, it is possible to test against metacall_value_id,
+    // or there is also a casting API for dealing with it.
+
+    // Clear parameters
+    args foreach metacall.metacall_value_destroy
+
+    // Clear return value
+    metacall metacall_value_destroy retPtr
+  }
+
+  "MetaCall" should "be destroyed successfully" in {
+    require(
+      metacall.metacall_destroy() == 0,
+      "MetaCall was not successfully destroyed"
+    )
   }
 }
+
+// This is a experiment I have been doing in order to implement a high level
+// abstraction for Scala. The objective is to make it as trasparent as possible,
+// like if you were dealing with normal scala values. Generics can help for sure.
+
+/*
+  trait ValueLifetime {
+    def finalize()
+  }
+
+  def finalizer [T, V <: ValueLifetime] (v : V) (f : V => T) : T =
+    try f(v)
+    finally v.finalize()
+
+  abstract class ValuePtr[T](v: Pointer) extends ValueLifetime {
+    def finalize() {
+      metacall.metacall_value_destroy(v)
+    }
+  }
+
+  class Value[@specialized(Int) T](i: Int) extends ValuePtr[T](metacall.metacall_value_create_int(i)) {
+    def to_value(): T = {
+      metacall.metacall_value_to_int(v)
+    }
+  }
+
+  class Value[@specialized(String) T](str: String) extends ValuePtr(metacall.metacall_value_create_string(i)) {
+
+  }
+ */
