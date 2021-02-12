101289c569f35130cc8058b4ec8783840cad9317
viferga <vic798@gmail.com>
2021-02-12 04:52:00 -0900

    Solve bugs from node port.

--- source/ports/node_port/index.js
+++ source/ports/node_port/index.js
@@ -214,34 +214,25 @@ mod.prototype.require = function (name) {
 	// 	/* Continue loading */
 	// }
 
-	/* Try to load it with NodeJS first */
-	try {
-		return node_require.apply(this, [ name ]);
-	} catch (e) {
-		if (e.code !== 'MODULE_NOT_FOUND') {
-			throw e;
-		}
-	}
-
-	try {
-		return node_require.apply(this, [ require.resolve(name) ]);
-	} catch (e) {
-		if (e.code !== 'MODULE_NOT_FOUND') {
-			throw e;
-		}
-	}
-
 	const index = name.lastIndexOf('.');
 
 	if (index !== -1) {
-		/* If there is extension, load the module depending on the tag */
-		const extension = name.substr(index + 1);
-		const tag = tags[extension];
+			/* If there is extension, load the module depending on the tag */
+			const extension = name.substr(index + 1);
+			const tag = tags[extension];
 
-		if (tag && tag !== 'node') {
-			/* Load with MetaCall if we found a tag and it is not NodeJS */
-			return metacall_require(tag, name);
-		}
+			if (tag && tag !== 'node') {
+					/* Load with MetaCall if we found a tag and it is not NodeJS */
+					return metacall_require(tag, name);
+			}
+	}
+
+	try {
+			return node_require.apply(this, [ name ]);
+	} catch (e) {
+			if (e.code !== 'MODULE_NOT_FOUND') {
+					throw e;
+			}
 	}
 
 	/* If there is no extension or the extension is not supported or it is 'node', load it with NodeJS require */
