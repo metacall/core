c6e57e5b033498e3816e6e94da5de50758a98637
viferga <vic798@gmail.com>
2021-02-13 06:33:29 -0900

    Add tests for fail paths in nodejs and python.

--- source/tests/metacall_node_fail_env_var_test/source/metacall_node_fail_env_var_test.cpp
+++ source/tests/metacall_node_fail_env_var_test/source/metacall_node_fail_env_var_test.cpp
@@ -23,49 +23,28 @@
 #include <metacall/metacall.h>
 #include <metacall/metacall_loaders.h>
 
-class metacall_python_fail_test : public testing::Test
+class metacall_node_fail_env_var_test : public testing::Test
 {
 public:
 };
 
-TEST_F(metacall_python_fail_test, DefaultConstructor)
+TEST_F(metacall_node_fail_env_var_test, DefaultConstructor)
 {
 	metacall_print_info();
 
 	ASSERT_EQ((int) 0, (int) metacall_initialize());
 
-	/* Python */
-	#if defined(OPTION_BUILD_LOADERS_PY)
+	/* NodeJS */
+	#if defined(OPTION_BUILD_LOADERS_NODE)
 	{
-		const char buffer[] =
-			"def sumList(list: [int]):\n"
-			"	return sum(list)\n";
-
-		// Apparently this loads the function but it does not understand the type
-		EXPECT_EQ((int) 0, (int) metacall_load_from_memory("py", buffer, sizeof(buffer), NULL));
-
-		enum metacall_value_id id;
-
-		EXPECT_EQ((int) 0, (int) metacall_function_parameter_type(metacall_function("sumList"), 0, &id));
-
-		// The type of list must be invalid once it loads
-		EXPECT_EQ((enum metacall_value_id) METACALL_INVALID, (enum metacall_value_id) id);
-
-		const char * py_scripts[] =
+		const char * node_scripts[] =
 		{
-			"this_does_not_exists_yeet.py"
+			"this_does_not_exists_yeet.js"
 		};
 
-		EXPECT_EQ((int) 1, (int) metacall_load_from_file("py", py_scripts, sizeof(py_scripts) / sizeof(py_scripts[0]), NULL));
-
-		const char buffer_fail[] =
-			"def sdf: as asf return";
-
-		// This must fail
-		EXPECT_EQ((int) 1, (int) metacall_load_from_memory("py", buffer_fail, sizeof(buffer_fail), NULL));
-
+		EXPECT_EQ((int) 1, (int) metacall_load_from_file("node", node_scripts, sizeof(node_scripts) / sizeof(node_scripts[0]), NULL));
 	}
-	#endif /* OPTION_BUILD_LOADERS_PY */
+	#endif /* OPTION_BUILD_LOADERS_NODE */
 
 	/* Print inspect information */
 	{
