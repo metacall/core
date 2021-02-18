60cbe683f486e1bd25431395f734dbf563e877e8
Vicente Ferrer <vic798@gmail.com>
2021-02-18 18:47:09 +0100

    Add more error checks in node port test.

--- source/ports/node_port/test/index.js
+++ source/ports/node_port/test/index.js
@@ -45,6 +45,13 @@ describe('metacall', () => {
 	describe('fail', () => {
 		it('require', () => {
 			assert.throws(() => { require('./asd.invalid') }, new Error('Cannot find module \'./asd.invalid\''));
+			// TODO: Improve error messages
+			assert.throws(() => { require('./asd.py') }, new Error('MetaCall could not load from file'));
+			assert.throws(() => { require('./asd.rb') }, new Error('MetaCall could not load from file'));
+			assert.throws(() => { require('./asd.cs') }, new Error('MetaCall could not load from file'));
+			// TODO: Implement TS with NodeJS compatibility
+			assert.throws(() => { require('./asd.ts') }, new Error('Cannot find module \'./asd.ts\''));
+			assert.throws(() => { require('./asd.tsx') }, new Error('Cannot find module \'./asd.tsx\''));
 		});
 	});
 
