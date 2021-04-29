metacall/core

commit 0a8e42675bae592d6b938a8197384dea2ad4a380

Solve bug of metacall_inspect called after the destroy of node loader has been issued. The destroy method of the node loader should be executed before any destruction of the loader structure itself.

source/loader/source/loader_impl.c
@@ -487,6 +487,8 @@ void loader_impl_destroy_handle(loader_handle_impl handle_impl)
 
 		if (handle_impl->impl->init == 0)
 		{
+			log_write("metacall", LOG_LEVEL_DEBUG, "Destroying handle %s", handle_impl->name);
+
 			if (loader_impl_function_hook_call(handle_impl->ctx, func_fini_name) != 0)
 			{
 				log_write("metacall", LOG_LEVEL_ERROR, "Error when calling destructor from handle impl: %p (%s)", (void *)handle_impl, func_fini_name);
@@ -1111,12 +1113,6 @@ void loader_impl_destroy(loader_impl impl)
 	{
 		log_write("metacall", LOG_LEVEL_DEBUG, "Destroy loader implementation %s", impl->tag);
 
-		set_iterate(impl->handle_impl_map, &loader_impl_destroy_handle_map_cb_iterate, NULL);
-
-		set_iterate(impl->type_info_map, &loader_impl_destroy_type_map_cb_iterate, NULL);
-
-		set_destroy(impl->type_info_map);
-
 		if (impl->init == 0)
 		{
 			loader_impl_interface interface_impl = loader_impl_symbol(impl);
@@ -1129,11 +1125,17 @@ void loader_impl_destroy(loader_impl impl)
 			impl->init = 1;
 		}
 
+		set_iterate(impl->handle_impl_map, &loader_impl_destroy_handle_map_cb_iterate, NULL);
+
+		set_destroy(impl->handle_impl_map);
+
+		set_iterate(impl->type_info_map, &loader_impl_destroy_type_map_cb_iterate, NULL);
+
+		set_destroy(impl->type_info_map);
+
 		set_iterate(impl->exec_path_map, &loader_impl_destroy_exec_path_map_cb_iterate, NULL);
 
 		set_destroy(impl->exec_path_map);
-
-		set_destroy(impl->handle_impl_map);
 
 		context_destroy(impl->ctx);
 

source/tests/metacall_node_python_async_after_destroy_test/source/metacall_node_python_async_after_destroy_test.cpp
@@ -42,7 +42,7 @@ TEST_F(metacall_node_python_async_after_destroy_test, DefaultConstructor)
 	{
 		static const char buffer[] =
 			/* NodeJS */
-			"const { metacall, metacall_load_from_memory } = require('" METACALL_NODE_PORT_PATH "');\n"
+			"const { metacall, metacall_load_from_memory, metacall_inspect } = require('" METACALL_NODE_PORT_PATH "');\n"
 			"metacall_load_from_memory('py', `"
 			/* Python */
 			"def sum(a, b):\n"
@@ -50,7 +50,7 @@ TEST_F(metacall_node_python_async_after_destroy_test, DefaultConstructor)
 			"`);\n"
 			"function log(x) { console.log(x); return x; }\n"
 			"setTimeout(() => { log(metacall('sum', 3, 4)) === 7 || process.exit(1) }, 2000);\n"
-			"setTimeout(() => { log(metacall('sum', 3, 4)) === 7 || process.exit(1) }, 4000);\n"
+			"setTimeout(() => { log(metacall_inspect()) || process.exit(1) }, 4000);\n"
 			"setTimeout(() => { log(metacall('sum', 3, 4)) === 7 || process.exit(1) }, 6000);\n";
 
 		ASSERT_EQ((int)0, (int)metacall_load_from_memory("node", buffer, sizeof(buffer), NULL));

commit da241bfb4e56086860c8a83945d9356f08271c38

Solve bug introduced by 0a8e42675bae592d6b938a8197384dea2ad4a380 ; this forced to executed the destroy events of the functions, classes and objects after the runtime is destroyed and this is very problematic. The destruction has been moved after the destroy children. Apparently this should not be a problem, but maybe this should be separated into two different functions if there is some special case of a loader. For now it works on node (which is the problematic one) and for the rest of loaders, which is exactly the same behavior as before.

source/loader/include/loader/loader.h
@@ -93,7 +93,7 @@ LOADER_API value loader_metadata(void);
 
 LOADER_API int loader_clear(void *handle);
 
-LOADER_API void loader_unload_children(void);
+LOADER_API void loader_unload_children(loader_impl impl);
 
 LOADER_API int loader_unload(void);
 

source/loader/include/loader/loader_impl.h
@@ -76,6 +76,8 @@ LOADER_API value loader_impl_metadata(loader_impl impl);
 
 LOADER_API int loader_impl_clear(void *handle);
 
+LOADER_API void loader_impl_destroy_objects(loader_impl impl);
+
 LOADER_API void loader_impl_destroy(loader_impl impl);
 
 LOADER_API loader_impl loader_impl_create_proxy(void);

source/loader/source/loader.c
@@ -806,7 +806,7 @@ int loader_clear(void *handle)
 	return loader_impl_clear(handle);
 }
 
-void loader_unload_children()
+void loader_unload_children(loader_impl impl)
 {
 	loader l = loader_singleton();
 	uint64_t current = thread_id_get_current();
@@ -847,6 +847,12 @@ void loader_unload_children()
 	}
 
 	vector_destroy(stack);
+
+	/* Clear all objects and types related to the loader once all childs have been destroyed */
+	if (impl != NULL)
+	{
+		loader_impl_destroy_objects(impl);
+	}
 }
 
 int loader_unload()
@@ -870,7 +876,7 @@ int loader_unload()
 			/* TODO: How to deal with this? */
 		}
 
-		loader_unload_children();
+		loader_unload_children(NULL);
 	}
 
 	/* Clear the implementation tag map */

source/loader/source/loader_impl.c
@@ -1107,6 +1107,27 @@ int loader_impl_destroy_exec_path_map_cb_iterate(set s, set_key key, set_value v
 	return 0;
 }
 
+void loader_impl_destroy_objects(loader_impl impl)
+{
+	/* This iterates through all functions, classes objects and types,
+	* it is necessary to be executed on demand because those can have
+	* implementations in the loader implementation which need to be GCed
+	* or freed properly before the runtime goes down but after the
+	* destroy has been issued, so while it is destroying, we can still
+	* retrieve the data for introspection or for whatever we need
+	*/
+	if (impl != NULL)
+	{
+		set_iterate(impl->handle_impl_map, &loader_impl_destroy_handle_map_cb_iterate, NULL);
+
+		set_destroy(impl->handle_impl_map);
+
+		set_iterate(impl->type_info_map, &loader_impl_destroy_type_map_cb_iterate, NULL);
+
+		set_destroy(impl->type_info_map);
+	}
+}
+
 void loader_impl_destroy(loader_impl impl)
 {
 	if (impl != NULL)
@@ -1124,14 +1145,6 @@ void loader_impl_destroy(loader_impl impl)
 
 			impl->init = 1;
 		}
-
-		set_iterate(impl->handle_impl_map, &loader_impl_destroy_handle_map_cb_iterate, NULL);
-
-		set_destroy(impl->handle_impl_map);
-
-		set_iterate(impl->type_info_map, &loader_impl_destroy_type_map_cb_iterate, NULL);
-
-		set_destroy(impl->type_info_map);
 
 		set_iterate(impl->exec_path_map, &loader_impl_destroy_exec_path_map_cb_iterate, NULL);
 

source/loaders/c_loader/source/c_loader_impl.cpp
@@ -289,7 +289,7 @@ int c_loader_impl_destroy(loader_impl impl)
 	if (c_impl != NULL)
 	{
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 
 		delete c_impl;
 

source/loaders/cob_loader/source/cob_loader_impl.cpp
@@ -259,7 +259,7 @@ int cob_loader_impl_destroy(loader_impl impl)
 	(void)impl;
 
 	// Destroy children loaders
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	return cobtidy();
 }

source/loaders/cs_loader/source/cs_loader_impl.c
@@ -396,7 +396,7 @@ int cs_loader_impl_destroy(loader_impl impl)
 	netcore_handle nhandle = (netcore_handle)loader_impl_get(impl);
 
 	/* Destroy children loaders */
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	simple_netcore_destroy(nhandle);
 

source/loaders/dart_loader/source/dart_loader_impl.cc
@@ -571,7 +571,7 @@ int dart_loader_impl_destroy(loader_impl impl)
 	if (dart_impl != nullptr)
 	{
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 
 		delete dart_impl;
 

source/loaders/file_loader/source/file_loader_impl.c
@@ -439,7 +439,7 @@ int file_loader_impl_destroy(loader_impl impl)
 	if (file_impl != NULL)
 	{
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 
 		free(file_impl);
 

source/loaders/java_loader/source/java_loader_impl.cpp
@@ -307,7 +307,7 @@ int java_loader_impl_destroy(loader_impl impl)
 		std::cout << "1ffffffffffffffffffffffffffff" << std::endl;
 
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 		std::cout << "2ffffffffffffffffffffffffffff" << std::endl;
 
 		java_impl->jvm->DestroyJavaVM();

source/loaders/js_loader/source/js_loader_impl.cpp
@@ -848,7 +848,7 @@ int js_loader_impl_destroy(loader_impl impl)
 	if (js_impl != nullptr)
 	{
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 
 		/* Destroy V8 */
 		if (js_impl->isolate_scope != nullptr)

source/loaders/jsm_loader/source/jsm_loader_impl.cpp
@@ -235,7 +235,7 @@ int jsm_loader_impl_destroy(loader_impl impl)
 	if (jsm_impl != NULL)
 	{
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 
 		/* Destroy Spider Monkey */
 		if (jsm_impl->cx != NULL)

source/loaders/llvm_loader/source/llvm_loader_impl.cpp
@@ -339,7 +339,7 @@ int llvm_loader_impl_destroy(loader_impl impl)
 	loader_impl_llvm llvm_impl = static_cast<loader_impl_llvm>(loader_impl_get(impl));
 
 	/* Destroy children loaders */
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	/* TODO */
 	// Clear here the resources, it must free all memory related to llvm_impl.

source/loaders/lua_loader/source/lua_loader_impl.c
@@ -507,7 +507,7 @@ int lua_loader_impl_destroy(loader_impl impl)
 	if (lua_impl != NULL)
 	{
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 
 		/* Destroy Lua VM */
 		lua_close(lua_impl->vm);

source/loaders/mock_loader/source/mock_loader_impl.c
@@ -571,7 +571,7 @@ int mock_loader_impl_destroy(loader_impl impl)
 	if (mock_impl != NULL)
 	{
 		/* Destroy children loaders */
-		loader_unload_children();
+		loader_unload_children(impl);
 
 		/* Destroy Mock */
 		free(mock_impl);

source/loaders/node_loader/source/node_loader_impl.cpp
@@ -256,6 +256,7 @@ struct loader_impl_node_type
 	int64_t extra_active_handles;
 	uv_prepare_t destroy_prepare;
 	std::atomic_bool event_loop_empty;
+	loader_impl impl;
 };
 
 typedef struct loader_impl_node_function_type
@@ -4006,6 +4007,9 @@ loader_impl_data node_loader_impl_initialize(loader_impl impl, configuration con
 	node_impl->result = 1;
 	node_impl->error_message = NULL;
 
+	/* Initialize the reference to the loader so we can use it on the destruction */
+	node_impl->impl = impl;
+
 /* Create NodeJS logging thread */
 #ifdef __ANDROID__
 	{
@@ -4576,7 +4580,7 @@ void node_loader_impl_destroy_safe_impl(loader_impl_node node_impl, napi_env env
 	napi_status status;
 
 	/* Destroy children loaders (any loader initialized in the current V8 thread should be deleted first) */
-	loader_unload_children();
+	loader_unload_children(node_impl->impl);
 
 	/* Clear thread safe functions */
 	{

source/loaders/py_loader/source/py_loader_impl.c
@@ -2288,7 +2288,7 @@ int py_loader_impl_destroy(loader_impl impl)
 	}
 
 	/* Destroy children loaders */
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	Py_DECREF(py_impl->inspect_signature);
 	Py_DECREF(py_impl->inspect_module);

source/loaders/rb_loader/source/rb_loader_impl.c
@@ -1441,7 +1441,7 @@ int rb_loader_impl_destroy(loader_impl impl)
 	(void)impl;
 
 	/* Destroy children loaders */
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	return ruby_cleanup(0);
 }

source/loaders/rpc_loader/source/rpc_loader_impl.cpp
@@ -646,7 +646,7 @@ int rpc_loader_impl_destroy(loader_impl impl)
 	loader_impl_rpc rpc_impl = (loader_impl_rpc)loader_impl_get(impl);
 
 	/* Destroy children loaders */
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	metacall_allocator_destroy(rpc_impl->allocator);
 

source/loaders/ts_loader/source/ts_loader_impl.cpp
@@ -374,7 +374,7 @@ int ts_loader_impl_destroy(loader_impl impl)
 	}
 
 	/* Destroy children loaders */
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	return metacall_clear(ts_impl);
 }

source/loaders/wasm_loader/source/wasm_loader_impl.c
@@ -214,7 +214,7 @@ int wasm_loader_impl_discover(loader_impl impl, loader_handle handle, context ct
 int wasm_loader_impl_destroy(loader_impl impl)
 {
 	/* Destroy children loaders */
-	loader_unload_children();
+	loader_unload_children(impl);
 
 	/* TODO */
 