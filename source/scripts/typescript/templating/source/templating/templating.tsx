5efbc1d1a622af582d1602b143118e72a2554dcf
viferga <vic798@gmail.com>
2021-05-12 01:55:21 -0800

    Add test for TSX templating with react (not working yet).

--- source/scripts/typescript/templating/source/templating/templating.tsx
+++ source/scripts/typescript/templating/source/templating/templating.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 
 export function hello(text: string): string {
-	return renderToString(<h1>Hello ${text}</h1>);
+	return renderToString(<h1>{`Hello ${text}`}</h1>);
 }
