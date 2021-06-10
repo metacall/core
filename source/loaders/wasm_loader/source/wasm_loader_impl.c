a13ba9989f189340d07a1778889966f83336c564
Encrylize <encrylize@gmail.com>
2021-06-10 18:29:07 +0200

    Add function discovery to Wasm loader

--- source/loaders/wasm_loader/source/wasm_loader_impl.c
+++ source/loaders/wasm_loader/source/wasm_loader_impl.c
@@ -136,6 +136,32 @@ function_interface function_wasm_singleton(void)
 	return &wasm_function_interface;
 }
 
+int wasm_loader_impl_initialize_types(loader_impl impl)
+{
+	// TODO: Implement ANYREF and FUNCREF?
+	static struct
+	{
+		type_id id;
+		const char *name;
+	} type_names[] = {
+		{ TYPE_INT, "i32" },
+		{ TYPE_LONG, "i64" },
+		{ TYPE_FLOAT, "f32" },
+		{ TYPE_DOUBLE, "f64" },
+	};
+
+	const size_t size = sizeof(type_names) / sizeof(type_names[0]);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		// TODO: Do we need error handling here?
+		type t = type_create(type_names[i].id, type_names[i].name, NULL, NULL);
+		loader_impl_type_define(impl, type_name(t), t);
+	}
+
+	return 0;
+}
+
 loader_impl_data wasm_loader_impl_initialize(loader_impl impl, configuration config)
 {
 	(void)config;
@@ -146,6 +172,12 @@ loader_impl_data wasm_loader_impl_initialize(loader_impl impl, configuration con
 	{
 		log_write("metacall", LOG_LEVEL_ERROR, "WebAssembly loader: Failed to allocate memory for loader implementation");
 		goto error_impl_alloc;
+	}
+
+	if (wasm_loader_impl_initialize_types(impl) != 0)
+	{
+		log_write("metacall", LOG_LEVEL_ERROR, "WebAssembly loader: Failed to initialize types");
+		goto error_types_init;
 	}
 
 	// TODO: Use wasm_engine_new_with_config function for passing in config?
@@ -165,9 +197,7 @@ loader_impl_data wasm_loader_impl_initialize(loader_impl impl, configuration con
 		goto error_store_creation;
 	}
 
-	/* Register initialization */
 	loader_initialization_register(impl);
-
 	log_write("metacall", LOG_LEVEL_DEBUG, "WebAssembly loader initialized correctly");
 
 	return wasm_impl;
@@ -175,6 +205,7 @@ loader_impl_data wasm_loader_impl_initialize(loader_impl impl, configuration con
 error_store_creation:
 	wasm_engine_delete(wasm_impl->engine);
 error_engine_creation:
+error_types_init:
 	free(wasm_impl);
 error_impl_alloc:
 	return NULL;
@@ -271,15 +302,116 @@ int wasm_loader_impl_clear(loader_impl impl, loader_handle handle)
 	return 0;
 }
 
-int wasm_loader_impl_discover(loader_impl impl, loader_handle handle, context ctx)
+type wasm_loader_impl_val_kind_to_type(loader_impl impl, wasm_valkind_t kind)
 {
-	/* TODO */
+	switch (kind)
+	{
+		case WASM_I32:
+			return loader_impl_type(impl, "i32");
+		case WASM_I64:
+			return loader_impl_type(impl, "i64");
+		case WASM_F32:
+			return loader_impl_type(impl, "f32");
+		case WASM_F64:
+			return loader_impl_type(impl, "f64");
+		default:
+			return NULL;
+	}
+}
 
-	(void)impl;
-	(void)handle;
-	(void)ctx;
+char *wasm_loader_impl_get_export_type_name(const wasm_exporttype_t *export_type)
+{
+	const wasm_name_t *name = wasm_exporttype_name(export_type);
+	char *null_terminated_name = malloc(name->size + 1);
+
+	if (null_terminated_name == NULL)
+	{
+		log_write("metacall", LOG_LEVEL_ERROR, "WebAssembly loader: Failed to allocate memory for name of export type");
+		return NULL;
+	}
+
+	strncpy(null_terminated_name, name->data, name->size);
+	null_terminated_name[name->size] = '\0';
+
+	return null_terminated_name;
+}
+
+int wasm_loader_impl_discover_function(loader_impl impl, scope scp, const wasm_externtype_t *extern_type, const char *name)
+{
+	const wasm_functype_t *func_type =
+		wasm_externtype_as_functype_const(extern_type);
+	const wasm_valtype_vec_t *params = wasm_functype_params(func_type);
+	const wasm_valtype_vec_t *results = wasm_functype_results(func_type);
+
+	// TODO: Add function implementation
+	function func = function_create(name, params->size, NULL, &function_wasm_singleton);
+	signature sig = function_signature(func);
+
+	if (results->size > 0)
+	{
+		// TODO: Add support for multiple return values
+		if (results->size > 1)
+		{
+			log_write("metacall", LOG_LEVEL_WARNING, "WebAssembly loader does not support multiple return values yet, using first return value");
+		}
+
+		signature_set_return(sig,
+			wasm_loader_impl_val_kind_to_type(impl, wasm_valtype_kind(results->data[0])));
+	}
+
+	for (size_t param_idx = 0; param_idx < params->size; param_idx++)
+	{
+		// TODO: We supply an empty string as the parameter name, since
+		//       `signature_set' doesn't allow `NULL'. Consider changing
+		//       the implementation of `signature_set` to allow this.
+		signature_set(sig, param_idx, "",
+			wasm_loader_impl_val_kind_to_type(impl, wasm_valtype_kind(params->data[param_idx])));
+	}
+
+	scope_define(scp, function_name(func), value_create_function(func));
 
 	return 0;
+}
+
+int wasm_loader_impl_discover(loader_impl impl, loader_handle handle, context ctx)
+{
+	int ret = 0;
+
+	loader_impl_wasm_handle wasm_handle = (loader_impl_wasm_handle)handle;
+	scope scp = context_scope(ctx);
+
+	// There is no way to check whether `wasm_module_exports` fails, so just
+	// hope for the best.
+	wasm_exporttype_vec_t export_types;
+	wasm_module_exports(wasm_handle->module, &export_types);
+
+	for (size_t export_idx = 0; export_idx < export_types.size; export_idx++)
+	{
+		// All of the `wasm_*` calls in this loop return pointers to memory
+		// owned by `export_types`, so no need to do any error checking or
+		// cleanup.
+		const wasm_externtype_t *extern_type = wasm_exporttype_type(export_types.data[export_idx]);
+		const wasm_externkind_t kind = wasm_externtype_kind(extern_type);
+		char *export_name = wasm_loader_impl_get_export_type_name(export_types.data[export_idx]);
+
+		if (export_name == NULL)
+		{
+			ret = 1;
+			goto cleanup;
+		}
+
+		// TODO: Do we need to implement the other types as well?
+		if (kind == WASM_EXTERN_FUNC)
+		{
+			wasm_loader_impl_discover_function(impl, scp, extern_type, export_name);
+		}
+
+		free(export_name);
+	}
+
+cleanup:
+	wasm_exporttype_vec_delete(&export_types);
+	return ret;
 }
 
 int wasm_loader_impl_destroy(loader_impl impl)
