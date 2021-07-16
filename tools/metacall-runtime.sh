b6c4601fb64b5b59387a33b43d863d77042f740b
Vicente Ferrer <vic798@gmail.com>
2021-07-16 15:23:31 +0200

    Update netcore to 5.0.8.

--- tools/metacall-runtime.sh
+++ tools/metacall-runtime.sh
@@ -128,7 +128,7 @@ sub_netcore5(){
 	rm packages-microsoft-prod.deb
 
 	$SUDO_CMD apt-get update
-	sub_apt_install_hold dotnet-runtime-5.0=5.0.7-1
+	sub_apt_install_hold dotnet-runtime-5.0=5.0.8-1
 }
 
 # V8
