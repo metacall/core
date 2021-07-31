a402cd743a9f890eaa7e5c193b8ac74751ed0ba8
viferga <vic798@gmail.com>
2021-07-31 02:58:07 -0800

    Solving problem with file (git seems not case sensitive) x2

---
+++ source/scripts/java/fibonacci/source/Fibonacci.java
@@ -0,0 +1,20 @@
+
+public class Fibonacci
+{
+	public static int fib_impl(int n)
+	{
+		if (n <= 2)
+		{
+			return 1;
+		}
+		else
+		{
+			return fib(n - 2) + fib(n - 1);
+		}
+	}
+
+	public static void fib(int n)
+	{
+		System.out.println("Fibbonaci of " + n + " = " + fib_impl(n));
+	}
+}
