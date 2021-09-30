23598b0ed04ca35e8b842e7e059dd94dd122a7a4
Raymond <devraymondsh@gmail.com>
2021-09-30 18:20:51 +0330

    Implement the LoaderLifecycleState

--- source/loaders/rs_loader/rust/src/lifecycle/initialize.rs
+++ source/loaders/rs_loader/rust/src/lifecycle/initialize.rs
@@ -1,9 +1,11 @@
-use crate::{c_int, c_void};
+use crate::{c_void, LoaderLifecycleState};
 
 #[no_mangle]
 pub extern "C" fn rs_loader_impl_initialize(
     _loader_impl: *mut c_void,
     _config: *mut c_void,
 ) -> *mut c_void {
-    1 as *const c_int as *mut c_void
+    let boxed_loader_lifecycle_state = Box::new(LoaderLifecycleState::new(Vec::new()));
+
+    Box::into_raw(boxed_loader_lifecycle_state) as *mut c_void
 }
