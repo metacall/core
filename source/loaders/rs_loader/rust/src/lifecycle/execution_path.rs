7432e3ecb86fc9389ae60a471e504b7938a9509b
Raymond <devraymondsh@gmail.com>
2021-09-30 19:21:36 +0330

    Get the LoaderLifecycleState and push path slices to it in the execution_path lifecycle

--- source/loaders/rs_loader/rust/src/lifecycle/execution_path.rs
+++ source/loaders/rs_loader/rust/src/lifecycle/execution_path.rs
@@ -1,12 +1,28 @@
-use crate::{c_char, c_int, c_void, CStr};
+use std::path::Path;
+
+use crate::{c_char, c_int, c_void, CStr, LoaderLifecycleState, PathBuf};
+
+extern "C" {
+    fn loader_impl_get(loader_impl: *mut c_void) -> *mut c_void;
+}
 
 #[no_mangle]
 pub extern "C" fn rs_loader_impl_execution_path(
-    _loader_impl: *mut c_void,
+    loader_impl: *mut c_void,
     path: *const c_char,
 ) -> c_int {
+    let loader_lifecycle_state =
+        unsafe { loader_impl_get(loader_impl) } as *mut LoaderLifecycleState;
+
     let c_path: &CStr = unsafe { CStr::from_ptr(path) };
+
     let path_slice: &str = c_path.to_str().unwrap();
-    println!("RUST: {}", path_slice);
+
+    unsafe {
+        (*(loader_lifecycle_state))
+            .paths
+            .push(PathBuf::from(path_slice));
+    }
+
     0 as c_int
 }
