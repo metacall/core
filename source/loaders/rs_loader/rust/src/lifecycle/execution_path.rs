3298e1f053422c188bf4418e1e033e637c439c07
Raymond <devraymondsh@gmail.com>
2021-09-30 14:05:06 +0330

    Format code and remove unused stuff

--- source/loaders/rs_loader/rust/src/lifecycle/execution_path.rs
+++ source/loaders/rs_loader/rust/src/lifecycle/execution_path.rs
@@ -1,9 +1,12 @@
-use crate::{ c_void, c_char, CStr, c_int };
+use crate::{c_char, c_int, c_void, CStr};
 
 #[no_mangle]
-pub extern "C" fn rs_loader_impl_execution_path(_loader_impl: *mut c_void, path: *const c_char) -> c_int {
-	let c_path: &CStr = unsafe { CStr::from_ptr(path) };
-	let path_slice: &str = c_path.to_str().unwrap();
-	println!("RUST: {}", path_slice);
-	0 as c_int
+pub extern "C" fn rs_loader_impl_execution_path(
+    _loader_impl: *mut c_void,
+    path: *const c_char,
+) -> c_int {
+    let c_path: &CStr = unsafe { CStr::from_ptr(path) };
+    let path_slice: &str = c_path.to_str().unwrap();
+    println!("RUST: {}", path_slice);
+    0 as c_int
 }
--- source/loaders/rs_loader/rust/src/lifecycle/load_from_file.rs
+++ source/loaders/rs_loader/rust/src/lifecycle/load_from_file.rs
@@ -1,16 +1,21 @@
-use crate::{ c_void, c_char, CStr, c_int };
+use crate::{c_char, c_int, c_void};
+// use crate::{c_char, c_int, c_void, CStr};
 
 #[no_mangle]
-pub extern "C" fn rs_loader_impl_load_from_file(_loader_impl: *mut c_void, paths: *const *mut c_char, size: usize) -> *mut c_void {
-	// TODO: Not working
-	/*
-	for i in 0 .. size {
-		let path: *const c_char = unsafe { *(paths.offset(i as isize)) };
-		let c_path: &CStr = unsafe { CStr::from_ptr(path) };
-		let path_slice: &str = c_path.to_str().unwrap();
-		println!("RUSTY: {}", path_slice);
-	}
-	*/
+pub extern "C" fn rs_loader_impl_load_from_file(
+    _loader_impl: *mut c_void,
+    _paths: *const *mut c_char,
+    _size: usize,
+) -> *mut c_void {
+    // TODO: Not working
+    /*
+    for i in 0 .. size {
+        let path: *const c_char = unsafe { *(paths.offset(i as isize)) };
+        let c_path: &CStr = unsafe { CStr::from_ptr(path) };
+        let path_slice: &str = c_path.to_str().unwrap();
+        println!("RUSTY: {}", path_slice);
+    }
+    */
 
-	1 as c_int as *mut c_void
+    1 as c_int as *mut c_void
 }
