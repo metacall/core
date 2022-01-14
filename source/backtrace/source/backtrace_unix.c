195ddfc7b74831cb4fad7e033c3df729775e1a92
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2022-01-14 12:03:53 +0100

    Solve minor bug in baccktracing unix.

--- source/backtrace/source/backtrace_unix.c
+++ source/backtrace/source/backtrace_unix.c
@@ -34,7 +34,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#ifdef(defined(__APPLE__) && defined(__MACH__)) || defined(__MACOSX__)
+#if (defined(__APPLE__) && defined(__MACH__)) || defined(__MACOSX__)
 	#include <sys/ucontext.h>
 #else
 	#include <ucontext.h>
