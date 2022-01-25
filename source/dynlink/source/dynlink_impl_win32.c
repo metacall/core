4a315cd0111a8f6f44833c99059c5a84b9c4a1b5
burnerlee <avi.aviral140@gmail.com>
2022-01-26 04:57:14 +0530

    add function for win32 systems

--- source/dynlink/source/dynlink_impl_win32.c
+++ source/dynlink/source/dynlink_impl_win32.c
@@ -29,7 +29,11 @@
 #include <string.h>
 
 #define WIN32_LEAN_AND_MEAN
+#include <psapi.h>
+#include <stdio.h>
+#include <tchar.h>
 #include <windows.h>
+#define METACALL_LIB_NAME "metacall"
 
 /* -- Methods -- */
 
@@ -89,6 +93,58 @@ int dynlink_impl_interface_unload_win32(dynlink handle, dynlink_impl impl)
 	return (FreeLibrary(impl) == FALSE);
 }
 
+static int str_ends_with(const char *s, const char *suffix)
+{
+	size_t slen = strlen(s);
+	size_t suffix_len = strlen(suffix);
+	return suffix_len <= slen && !strcmp(s + slen - suffix_len, suffix);
+}
+
+static char *metacall_custom_lib_path(char *metacall_lib_path, char *metacall_lib_name)
+{
+	size_t lib_path_len = strlen(metacall_lib_path) - (strlen(metacall_lib_name) + 1);
+	char *custom_lib_path = malloc(sizeof(char) * (lib_path_len + 1));
+	custom_lib_path[lib_path_len] = 0;
+	strncpy(custom_lib_path, metacall_lib_path, lib_path_len);
+	return custom_lib_path;
+}
+
+// TODO: free returned string pointer
+const char *dynlink_impl_interface_get_metacall_lib_path_win32(void)
+{
+#if (!defined(NDEBUG) || defined(DEBUG) || defined(_DEBUG) || defined(__DEBUG) || defined(__DEBUG__))
+	static char metacall_lib_name[17] = METACALL_LIB_NAME "d.dll";
+#else
+	static char metacall_lib_name[16] = METACALL_LIB_NAME ".dll";
+#endif
+	HMODULE handle_modules[1024];
+	HANDLE handle_process;
+	DWORD cb_needed;
+	unsigned int i;
+	handle_process = GetCurrentProcess();
+
+	char *metacall_lib_path;
+	if (EnumProcessModules(handle_process, handle_modules, sizeof(handle_modules), &cb_needed))
+	{
+		for (i = 0; i < (cb_needed / sizeof(HMODULE)); i++)
+		{
+			TCHAR lib_path[MAX_PATH];
+
+			// Get the full path to the module's file.
+
+			if (GetModuleFileNameEx(handle_process, handle_modules[i], lib_path,
+					sizeof(lib_path) / sizeof(TCHAR)))
+			{
+				if (str_ends_with(lib_path, metacall_lib_name))
+				{
+					metacall_lib_path = metacall_custom_lib_path(lib_path, metacall_lib_name);
+				}
+			}
+		}
+	}
+	return metacall_lib_path;
+}
+
 dynlink_impl_interface dynlink_impl_interface_singleton_win32(void)
 {
 	static struct dynlink_impl_interface_type impl_interface_win32 = {
@@ -96,7 +152,8 @@ dynlink_impl_interface dynlink_impl_interface_singleton_win32(void)
 		&dynlink_impl_interface_get_name_win32,
 		&dynlink_impl_interface_load_win32,
 		&dynlink_impl_interface_symbol_win32,
-		&dynlink_impl_interface_unload_win32
+		&dynlink_impl_interface_unload_win32,
+		&dynlink_impl_interface_get_metacall_lib_path_win32
 	};
 
 	return &impl_interface_win32;
