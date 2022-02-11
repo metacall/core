a02b9450fa007e05adb4f8e2e74abb6ecad00568
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2022-02-11 01:18:58 +0100

    Update copyright.

--- cmake/FindV8.cmake
+++ cmake/FindV8.cmake
@@ -2,7 +2,7 @@
 #	CMake Find V8 Google JavaScript Engine by Parra Studios
 #	CMake script to find V8 JavaScript Engine.
 #
-#	Copyright (C) 2016 - 2021 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+#	Copyright (C) 2016 - 2022 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
 #
 #	Licensed under the Apache License, Version 2.0 (the "License");
 #	you may not use this file except in compliance with the License.
