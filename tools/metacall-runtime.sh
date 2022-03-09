65df57d7a28773f9609258b7d8415cebba8c7ed4
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2022-03-09 13:07:44 +0200

    Update NetCore version.

--- tools/metacall-runtime.sh
+++ tools/metacall-runtime.sh
@@ -114,7 +114,7 @@ sub_netcore5(){
 	rm packages-microsoft-prod.deb
 
 	$SUDO_CMD apt-get update
-	sub_apt_install_hold dotnet-runtime-5.0=5.0.14-1
+	sub_apt_install_hold dotnet-runtime-5.0=5.0.15-1
 }
 
 # V8
