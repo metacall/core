765f180334310f3ff0327af83d082badd99e439c
Samyak S Sarnayak <samyak201@gmail.com>
2022-04-13 23:26:08 +0530

    rust port: use macro to generate inline macros
    The code for each language specific proc macro (`py!{}`, `node!{}`,
    etc.) was being generated in a build script by substituting the name
    in a string (`format!()`), saving the generated code in a file and
    then loading it back in the code.
    
    This was unnecessary as the code can be generated through a
    declarative macro (a.k.a. "macro by example").
    
    So the following changes were made:
    - Defined a new macro `gen_inline_macro` which generates an inline proc
      macro for the given language
    - This does not use any funny string interpolation
    - Called this for every language defined in the build script
    - Removed build script as it was only used to generate the code
    
    Note that the usage remains the same - we get `py!{}`, `node!{}`, etc.
    macros as before.
    
    Also removed `extern crate proc_macro2;` because the `extern crate`
    declaration is not needed in newer Rust versions.

--- source/ports/rs_port/inline/src/lib.rs
+++ source/ports/rs_port/inline/src/lib.rs
@@ -1,7 +1,28 @@
-extern crate proc_macro2;
-
 use proc_macro::TokenStream;
 use quote::quote;
 
-// include generated macros
-include!(concat!(env!("OUT_DIR"), "/inline_gen.rs"));
+macro_rules! gen_inline_macro {
+    ($name:ident) => (
+        #[proc_macro]
+        pub fn $name(input: TokenStream) -> TokenStream {
+            let token_stream_input = proc_macro2::TokenStream::from(input);
+            let buffer = token_stream_input.to_string();
+
+            let result = quote! {{
+                ::metacall::load_from_memory("$name", #buffer.to_string()).unwrap()
+            }};
+
+            result.into()
+        }
+    )
+}
+
+gen_inline_macro!(py);
+gen_inline_macro!(node);
+gen_inline_macro!(ts);
+gen_inline_macro!(cs);
+gen_inline_macro!(rb);
+gen_inline_macro!(cob);
+gen_inline_macro!(rpc);
+gen_inline_macro!(java);
+gen_inline_macro!(wasm);
