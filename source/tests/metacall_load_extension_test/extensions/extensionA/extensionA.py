cfa289926a189162da1d98d66caf9de3705fb46f
rxbryan <rxbryn@gmail.com>
2022-06-27 00:17:37 +0100

    add test for load_extension

---
+++ source/tests/metacall_load_extension_test/extensions/extensionA/extensionA.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def extensionA():
+	print('Hello World from extensionA!!')
+	return
+
---
+++ source/tests/metacall_load_extension_test/extensions/extensionC/extensionC.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+function extensionC() {
+	console.log('Hello World, from extensionC');
+	return
+}
+
+
+module.exports = {
+	extensionC
+};
