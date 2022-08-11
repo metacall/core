76543d07da52f3b3bcf24576b53a260ea927e980
Tricster <mediosrity@gmail.com>
2022-08-11 09:01:47 +0800

    Handle build type in rs_port.
    Signed-off-by: Tricster <mediosrity@gmail.com>

--- source/ports/rs_port/build.rs
+++ source/ports/rs_port/build.rs
@@ -3,8 +3,21 @@ fn main() {
     // when running tests
     if let Ok(val) = env::var("CMAKE_BINARY_DIR") {
         println!("cargo:rustc-link-search={val}");
-        // try to link the debug version when running tests
-        println!("cargo:rustc-link-lib=metacalld");
+
+        match env::var("CMAKE_BUILD_TYPE") {
+            Ok(val) => {
+                if val == "Debug" {
+                    // try to link the debug version when running tests
+                    println!("cargo:rustc-link-lib=metacalld");
+                } else {
+                    println!("cargo:rustc-link-lib=metacall");
+                }
+            }
+            Err(_) => {
+                println!("cargo:rustc-link-lib=metacall");
+            }
+        }
+
         println!("cargo:rustc-env=LD_LIBRARY_PATH={val}");
         println!("cargo:rustc-env=CONFIGURATION_PATH={val}/configurations/global.json")
     } else {
