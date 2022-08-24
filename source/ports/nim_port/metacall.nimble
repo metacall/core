d13d5bc5611875834a88c6a4e5ee59169966b4aa
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2022-08-24 23:26:07 +0200

    Add base for Nim port packaging.

---
+++ source/ports/nim_port/metacall.nimble
@@ -0,0 +1,11 @@
+# Package
+
+version       = "0.1.0"
+author        = "Vicente Eduardo Ferrer Garcia"
+description   = "MetaCall Nim Port (library for interoperability between Nim and multiple programming languages)"
+license       = "Apache-2.0"
+srcDir        = "src"
+
+# Dependencies
+
+requires "nim >= 1.6.0"
--- source/ports/nim_port/src/metacall.nim
+++ source/ports/nim_port/src/metacall.nim
@@ -1,5 +1,10 @@
 {.passL: "-lmetacall".}
 
+# TODO: Add library and header detection via https://nim-lang.org/docs/system.html#staticExec%2Cstring%2Cstring%2Cstring
+# and conditional compilation: https://forum.nim-lang.org/t/1260#7772 & https://forum.nim-lang.org/t/867#5148
+# Check if it is being compiled under Linux and check if the installation is done by using distributable-linux, then
+# add the following flags: https://github.com/metacall/nim-javascript-example/blob/e30288f3342811110bb94dc76b21ef8396d7c224/Dockerfile#L48
+
 {.pragma: metacallHeader, header: "metacall/metacall.h".}
 {.pragma: metacallValueHeader, header: "metacall/metacall_value.h".}
 {.pragma: metacallProc, metacallHeader, metacallValueHeader, importc: "metacall_$1".}
@@ -39,5 +44,5 @@ proc value_destroy*(v: pointer): void {.metacallProc.}
 proc initialize*(): cint {.metacallProc.}
 proc load_from_file*(tag: cstring, paths: cstringArray, size: csize_t, handle: pointer): cint {.metacallProc.}
 proc load_from_memory*(tag: cstring, buffer: cstring, size: csize_t, handle: pointer): cint {.metacallProc.}
-proc v_s*(name: cstring, args: pointer, size: csize_t): pointer {.metacallCallProc.}
+proc v_s*(name: cstring, args: pointer, size: csize_t): pointer {.metacallCallProc.} # TODO: Improve this with macros, maybe metaprogramming
 proc destroy*(): cint {.metacallProc.}
