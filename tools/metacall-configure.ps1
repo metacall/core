a23370c30b61beb593f4a142375d37c95c67441e
Param Siddharth <contact@paramsid.com>
2022-09-06 01:24:53 +0530

    Syntactical fix
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- tools/metacall-configure.ps1
+++ tools/metacall-configure.ps1
@@ -137,7 +137,7 @@ function sub-configure {
 	$Global:BUILD_STRING = "-DOPTION_BUILD_LOG_PRETTY=Off " `
 			+ "-DOPTION_BUILD_LOADERS=On "                  `
 			+ "-DOPTION_BUILD_LOADERS_MOCK=On "             `
-			+ "-G ""NMake Makefiles""
+			+ "-G ""NMake Makefiles"""
 
 	# Scripts
 	if ( $BUILD_SCRIPTS -eq 1 ) {
