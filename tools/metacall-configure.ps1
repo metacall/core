fad47801a2701f3c3ab5e712442ba2c3de8d9887
Param Siddharth <contact@paramsid.com>
2022-09-07 21:56:53 +0530

    Generate no NMake Makefiles
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- tools/metacall-configure.ps1
+++ tools/metacall-configure.ps1
@@ -136,8 +136,7 @@ function sub-options {
 function sub-configure {
 	$Global:BUILD_STRING = "-DOPTION_BUILD_LOG_PRETTY=Off " `
 			+ "-DOPTION_BUILD_LOADERS=On "                  `
-			+ "-DOPTION_BUILD_LOADERS_MOCK=On "             `
-			+ "-G ""NMake Makefiles"""
+			+ "-DOPTION_BUILD_LOADERS_MOCK=On"
 
 	# Scripts
 	if ( $BUILD_SCRIPTS -eq 1 ) {
