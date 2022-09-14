97c183d3575e1b358d3b3fb3d0a0af63fb032eba
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2022-09-15 00:11:35 +0200

    Update python port version.

--- source/ports/py_port/setup.py
+++ source/ports/py_port/setup.py
@@ -39,7 +39,7 @@ options = {
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	'version': '0.4.2',
+	'version': '0.5.0',
 
 	'description': 'A library for providing inter-language foreign function interface calls',
 	'long_description': long_description,
