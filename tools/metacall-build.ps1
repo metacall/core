30eff04a19db6f6b9df4dbee983b8f281a7dd99f
Param Siddharth <contact@paramsid.com>
2022-09-25 16:17:24 +0530

    Copy Ruby DLL (too) after build finishes
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- tools/metacall-build.ps1
+++ tools/metacall-build.ps1
@@ -49,8 +49,8 @@ function sub-build {
 		# Prerequisites
 		$files = @(
 			"..\runtimes\nodejs\lib\libnode.dll",
-			"..\runtimes\nodejs\lib\libnode.lib"
-			# "..\runtimes\ruby\bin\x64-vcruntime140-ruby310.dll"
+			"..\runtimes\nodejs\lib\libnode.lib",
+			"..\runtimes\ruby\bin\x64-vcruntime140-ruby310.dll"
 		)
 
 		ForEach ($file in $files) {
