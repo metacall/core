ac19a34bf34ee6b3100309dc5165b4ac4c37000e
Praveen Kumar <pkspyder007@gmail.com>
2023-01-06 10:09:12 +0530

    copy dependencies for windows test

--- tools/metacall-build.ps1
+++ tools/metacall-build.ps1
@@ -56,9 +56,12 @@ function sub-build {
 
 		# Prerequisites
 		$files = @(
-			"..\runtimes\nodejs\lib\libnode.dll",
-			"..\runtimes\nodejs\lib\libnode.lib",
-			"..\runtimes\ruby\bin\x64-vcruntime140-ruby310.dll"
+			".\runtimes\nodejs\lib\libnode.dll",
+			".\runtimes\nodejs\lib\libnode.lib",
+			".\runtimes\ruby\bin\x64-vcruntime140-ruby310.dll",
+			".\source\loaders\node_loader\bootstrap\acorn",
+			".\source\loaders\node_loader\bootstrap\acorn.cmd",
+			".\source\loaders\node_loader\bootstrap\acorn.ps1"
 		)
 
 		ForEach ($file in $files) {
@@ -67,6 +70,14 @@ function sub-build {
 				cp $file ".\$BUILD_TYPE\"
 			}
 		}
+
+		# copy scripts and node_module for test
+		$nmPath = ".\source\loaders\node_loader\bootstrap\node_modules"
+		$scriptsPath = ".\scripts"
+
+		robocopy /e "$nmPath" ".\$BUILD_TYPE\node_modules" /NFL /NDL /NJH /NJS /NC /NS /NP
+		robocopy /e "$scriptsPath" ".\$BUILD_TYPE\scripts" /NFL /NDL /NJH /NJS /NC /NS /NP
+		
 
 		ctest "-j$((Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors)" --output-on-failure -C $BUILD_TYPE
 
