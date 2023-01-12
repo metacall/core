b9a29747497b1ab1c415d431a02fb3a1c2c69d4d
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2023-01-12 21:55:24 +0100

    Trying to solve bugs in node port.

--- source/ports/node_port/index.d.ts
+++ source/ports/node_port/index.d.ts
@@ -4,6 +4,8 @@ declare module 'metacall' {
 	export function metacall_load_from_file_export(tag: string, paths: string[]): any;
 	export function metacall_load_from_memory(tag: string, code: string): number;
 	export function metacall_load_from_memory_export(tag: string, code: string): any;
+	export function metacall_load_from_configuration(path: string): number;
+	export function metacall_load_from_configuration_export(path: string): any;
 	export function metacall_inspect(): any;
 	export function metacall_handle(tag: string, name: string): any;
 	export function metacall_logs(): void;
