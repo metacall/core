610b394d69a02bb5744600943ffb39b290e86395
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2023-01-22 17:28:24 +0100

    Solve bug in reference count.

--- source/threading/include/threading/threading_atomic_ref_count.h
+++ source/threading/include/threading/threading_atomic_ref_count.h
@@ -70,7 +70,7 @@ inline int threading_atomic_ref_count_increment(threading_atomic_ref_count ref)
 		return 1;
 	}
 
-	atomic_fetch_add_explicit(&ref->count, 1U, memory_order_relaxed);
+	atomic_fetch_add_explicit(&ref->count, 1, memory_order_relaxed);
 
 	return 0;
 }
@@ -82,7 +82,7 @@ inline int threading_atomic_ref_count_decrement(threading_atomic_ref_count ref)
 		return 1;
 	}
 
-	uintmax_t old_ref_count = atomic_fetch_sub_explicit(&ref->count, 1U, memory_order_release);
+	uintmax_t old_ref_count = atomic_fetch_sub_explicit(&ref->count, 1, memory_order_release);
 
 	if (old_ref_count == THREADING_ATOMIC_REF_COUNT_MIN + 1)
 	{
