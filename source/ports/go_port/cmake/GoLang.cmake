a1a079f352d3c85c0516207542bd98243e70842f
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2023-03-28 01:43:30 +0200

    Improve go port support and add testing with cmake.

--- source/ports/go_port/cmake/GoLang.cmake
+++ source/ports/go_port/cmake/GoLang.cmake
@@ -1,13 +1,15 @@
 set(GOPATH "${CMAKE_CURRENT_BINARY_DIR}/go")
 file(MAKE_DIRECTORY ${GOPATH})
 
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/CMakeDetermineGoCompiler.cmake)
+
 function(ExternalGoProject_Add TARG)
-	add_custom_target(${TARG} env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} get ${ARGN})
+	add_custom_target(${TARG} ${CMAKE_COMMAND} -E env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} get ${ARGN})
 endfunction(ExternalGoProject_Add)
 
 function(add_go_executable NAME)
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/.timestamp 
-		COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
+		COMMAND ${CMAKE_COMMAND} -E env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build
 		-o "${PROJECT_OUTPUT_DIR}/${NAME}"
 		${CMAKE_GO_FLAGS} ${ARGN}
 		WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
@@ -18,19 +20,23 @@ endfunction(add_go_executable)
 
 function(add_go_library NAME BUILD_TYPE)
 	if(BUILD_TYPE STREQUAL "STATIC")
-		set(BUILD_MODE -buildmode=c-archive)
-		set(LIB_NAME "lib${NAME}.a")
+		if(WIN32)
+			set(LIB_NAME "lib${NAME}.lib")
+		else()
+			set(LIB_NAME "lib${NAME}.a")
+		endif()
 	else()
-		set(BUILD_MODE -buildmode=c-shared)
 		if(APPLE)
 			set(LIB_NAME "lib${NAME}.dylib")
+		elseif(WIN32)
+			set(LIB_NAME "lib${NAME}.dll")
 		else()
 			set(LIB_NAME "lib${NAME}.so")
 		endif()
 	endif()
 
 	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/.timestamp
-		COMMAND env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
+		COMMAND ${CMAKE_COMMAND} -E env GOPATH=${GOPATH} ${CMAKE_Go_COMPILER} build ${BUILD_MODE}
 		-o "${PROJECT_OUTPUT_DIR}/${LIB_NAME}"
 		${CMAKE_GO_FLAGS} ${ARGN}
 		WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
