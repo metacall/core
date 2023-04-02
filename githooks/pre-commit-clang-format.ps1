94271e3d599cc633f7d0919a05b48a66a92370e1
Manan Garg <manantechy@gmail.com>
2023-04-02 10:42:59 +0530

    fix error in ported find_clang_format

--- githooks/pre-commit-clang-format.ps1
+++ githooks/pre-commit-clang-format.ps1
@@ -1,12 +1,14 @@
 $RECOMMENDED_CLANG_FORMAT_MAJOR="11"
 
 function Get-ClangFormatVersion {
-    $program = $(Get-Command clang-format -ErrorAction SilentlyContinue)
-    $clangFormatVersion = $program.Version
-    $program = $program.Source
-    $clangMajorVersion = $clangFormatVersion.Split(" ")[2].Split(".")[0]
-    if ($clangMajorVersion -ge $RECOMMENDED_CLANG_FORMAT_MAJOR) {
-        return $program
+    $program = $(Get-Command clang-format.exe -ErrorAction SilentlyContinue)
+    if ($program) {
+        $version = & $program.Source --version
+        $clangFormatVersion = $version.Split(" ")[2]
+        $clangMajorVersion = $clangFormatVersion.Split(".")[0]
+        if ($clangMajorVersion -ge $RECOMMENDED_CLANG_FORMAT_MAJOR) {
+            return $clangMajorVersion
+        }
     }
 }
 
