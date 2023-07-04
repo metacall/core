3ea1da80ade0b00f95cac9d3a99b3f70f0c64495
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2023-07-05 00:02:35 +0200

    Update wasm tests.

--- source/tests/metacall_wasm_test/source/metacall_wasm_test.cpp
+++ source/tests/metacall_wasm_test/source/metacall_wasm_test.cpp
@@ -182,12 +182,11 @@ TEST_F(metacall_wasm_test, CallFunctions)
 	ASSERT_EQ((double)4.0, (double)metacall_value_to_double(values[3]));
 	metacall_value_destroy(ret);
 
-	// // It should exit with illegal instruction
-	// #if defined(unix) || defined(__unix__) || defined(__unix) || defined(linux) || defined(__linux__) || defined(__linux) || defined(__gnu_linux)
-	// ASSERT_EXIT((metacall("trap"), exit(0)), ::testing::KilledBySignal(SIGILL), ".*");
-	// #endif
-
-	ASSERT_EQ((void *)NULL, metacall("trap"));
+	// It should exit with illegal instruction
+	#if defined(unix) || defined(__unix__) || defined(__unix) || \
+		defined(linux) || defined(__linux__) || defined(__linux) || defined(__gnu_linux)
+	ASSERT_EXIT((metacall("trap"), exit(0)), ::testing::KilledBySignal(SIGILL), ".*");
+	#endif
 }
 
 TEST_F(metacall_wasm_test, LinkModules)
