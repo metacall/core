67ea7cd85f091ac3397b359fb7be7da4dc3b4b8a
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2023-09-13 18:59:24 +0200

    Add sanitizer for the benchmarks.

--- tools/metacall-benchmarks-merge.py
+++ tools/metacall-benchmarks-merge.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
 import json
 import os
-from sys import argv
+import sys
 
 # Validate arguments
-if len(argv) != 2:
+if len(sys.argv) != 2:
 	print('Invalid number of arguments, you should pass the location of the benchmarks.')
 	print('Usage: python3 metacall-benchmarks-merge.py ${CMAKE_BINARY_DIR}/benchmarks')
 	exit(1)
 
 # Validate the benchmark path
-if not os.path.isdir(argv[1]):
-	print('The directory \'' + argv[1] + '\' does not exist or is not a valid path.')
+if not os.path.isdir(sys.argv[1]):
+	print('The directory \'' + sys.argv[1] + '\' does not exist or is not a valid path.')
 	exit(2)
 
 # Search all benchmarks and merge them
 output = {}
 
-for file in os.listdir(argv[1]):
+for file in os.listdir(sys.argv[1]):
 	if file.endswith('.json'):
-		f = open(os.path.join(argv[1], file), 'r')
-		data = json.loads(f.read())
+		f = open(os.path.join(sys.argv[1], file), 'r')
+
+		# Sanitize the data (https://github.com/google/benchmark/issues/784)
+		# '-Infinity', 'Infinity', 'NaN'
+		def sanitize(arg):
+			c = {
+				'-Infinity': sys.float_info.min, # -float('inf')
+				'Infinity': sys.float_info.max, # float('inf')
+				'NaN': 0 # float('nan')
+			}
+			# TODO: Eventually solve this from the root of problem in Windows
+			print('Warning: Got value "' + arg + '", review where the test is failing')
+			return c[arg]
+
+		data = json.loads(f.read(), parse_constant=sanitize)
 
 		if not output:
 			output = data
@@ -32,14 +45,14 @@ for file in os.listdir(argv[1]):
 
 # Check if we found data
 if not output:
-	print('The directory \'' + argv[1] + '\' does not contain any benchmark.')
+	print('The directory \'' + sys.argv[1] + '\' does not contain any benchmark.')
 	exit(3)
 
 # Rename the binary file
 output['context']['executable'] = 'benchmarks'
 
 # Store the result
-dest = os.path.join(argv[1], 'metacall-benchmarks.json')
+dest = os.path.join(sys.argv[1], 'metacall-benchmarks.json')
 f = open(dest, 'w+')
 json.dump(output, f, indent = 4)
 f.close()
