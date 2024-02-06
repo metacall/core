fc1c3af5f16f587158c72e5da6ab444dd8225a3c
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-02-06 23:41:58 +0100

    Add stack trace support in logs when address sanitizer is enabled.

--- source/log/source/log_impl.c
+++ source/log/source/log_impl.c
@@ -29,6 +29,10 @@
 
 #include <stdlib.h>
 
+#if defined(__ADDRESS_SANITIZER__) || defined(__THREAD_SANITIZER__) || defined(__MEMORY_SANITIZER__)
+	#include <sanitizer/common_interface_defs.h>
+#endif
+
 /* -- Member Data -- */
 
 struct log_impl_type
@@ -117,10 +121,17 @@ void log_impl_verbosity(log_impl impl, enum log_level_id level)
 int log_impl_write(log_impl impl, const log_record_ctor record_ctor)
 {
 	log_aspect stream = log_impl_aspect(impl, LOG_ASPECT_STREAM);
-
 	log_aspect_stream_impl stream_impl = log_aspect_derived(stream);
+	int result = stream_impl->write(stream, record_ctor);
 
-	if (stream_impl->write(stream, record_ctor) != 0)
+#if defined(__ADDRESS_SANITIZER__) || defined(__THREAD_SANITIZER__) || defined(__MEMORY_SANITIZER__)
+	if (record_ctor->level >= LOG_LEVEL_WARNING /* TODO: Check if stream is stdout */)
+	{
+		__sanitizer_print_stack_trace();
+	}
+#endif
+
+	if (result != 0)
 	{
 		return 1;
 	}
