4da180e7213f615e7b485528137a9b9443f1b264
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-06-19 19:39:00 +0200

    Solve issues.

--- tools/metacall-runtime.sh
+++ tools/metacall-runtime.sh
@@ -265,8 +265,8 @@ sub_c(){
 
 			case ${LINUX_DISTRO} in
 				debian)
-					# For now trixie == sid, change when trixie is released
-					if [ "${VERSION:-}" = "unstable" ] || [ "${VERSION:-}" = "testing" ] || [ "${VERSION_CODENAME}" = "trixie" ]; then
+					# For now bookworm || trixie == sid, change when trixie is released
+					if [ "${VERSION:-}" = "unstable" ] || [ "${VERSION:-}" = "testing" ] || [ "${VERSION_CODENAME}" = "bookworm" ] || [ "${VERSION_CODENAME}" = "trixie" ]; then
 						CODENAME="unstable"
 						LINKNAME=""
 					else
