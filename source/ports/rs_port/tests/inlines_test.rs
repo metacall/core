91b015dcf3b3409830d25abe69be41b06c566aed
hulxv <hulxxv@gmail.com>
2024-07-10 02:23:57 +0300

    test: adding test cases for incorrect size of script

--- source/ports/rs_port/tests/inlines_test.rs
+++ source/ports/rs_port/tests/inlines_test.rs
@@ -12,16 +12,25 @@ fn inlines() {
             print("hello world")
         }
     }
+    if loaders::from_memory("py", "").is_ok() {
+        py! {print("hello world")}
+    }
 
     if loaders::from_memory("node", "").is_ok() {
         node! {
             console.log("hello world");
         }
     }
+    if loaders::from_memory("node", "").is_ok() {
+        node! {console.log("hello world")}
+    }
 
     if loaders::from_memory("ts", "").is_ok() {
         ts! {
             console.log("hello world");
         }
+    }
+    if loaders::from_memory("ts", "").is_ok() {
+        ts! {console.log("hello world")}
     }
 }
