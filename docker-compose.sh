91ab530298a10d82b5541af78e1c5ec8666de747
Yasindu Dissanayake <23ug1-0099@sltc.ac.lk>
2024-07-21 10:17:19 +0530

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -66,6 +66,15 @@ sub_build() {
 
 	ln -sf tools/cli/.dockerignore .dockerignore
 	$DOCKER_COMPOSE -f docker-compose.yml build --force-rm cli
+
+	# Multi-architecture build setup
+	docker buildx create --use --name multiarch-builder
+    	docker buildx inspect --bootstrap
+
+    	$DOCKER_COMPOSE -f docker-compose-multiarch.yml build
+    	$DOCKER_COMPOSE -f docker-compose-multiarch.yml push
+
+    	docker buildx rm multiarch-builder
 }
 
 # Build MetaCall Docker Compose without cache (link manually dockerignore files)
