0a09f82157be9f0ec3cd55060f224fe002f0f0dc
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-08-14 03:18:27 +0200

    Solve issue with runtime on ubuntu noble.

--- tools/metacall-runtime.sh
+++ tools/metacall-runtime.sh
@@ -226,7 +226,7 @@ sub_rpc(){
 					;;
 			esac
 
-			if [ "${CODENAME}" = "trixie" ] || [ "${CODENAME}" = "unstable" ]; then
+			if [ "${CODENAME}" = "trixie" ] || [ "${CODENAME}" = "noble" ] || [ "${CODENAME}" = "unstable" ]; then
 				sub_apt_install_hold libcurl4t64
 			else
 				sub_apt_install_hold libcurl4
@@ -345,7 +345,7 @@ sub_backtrace(){
 					;;
 			esac
 
-			if [ "${CODENAME}" = "trixie" ] || [ "${CODENAME}" = "unstable" ]; then
+			if [ "${CODENAME}" = "trixie" ] || [ "${CODENAME}" = "noble" ] || [ "${CODENAME}" = "unstable" ]; then
 				sub_apt_install_hold libdw1t64 libelf1t64
 			else
 				sub_apt_install_hold libdw1
