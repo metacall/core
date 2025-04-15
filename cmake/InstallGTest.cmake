8b20384e3ad253f62aa603c445f280e3fbeb3c49
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2025-04-15 20:33:09 +0200

    Solve issues with gtest.

--- cmake/InstallGTest.cmake
+++ cmake/InstallGTest.cmake
@@ -38,7 +38,7 @@ if(NOT GTEST_FOUND OR USE_BUNDLED_GTEST)
 	# Import Google Test Framework
 	ExternalProject_Add(google-test-depends
 		GIT_REPOSITORY https://github.com/google/googletest.git
-		GIT_TAG release-${GTEST_VERSION}
+		GIT_TAG v${GTEST_VERSION}
 		CMAKE_ARGS
 			-Dgtest_build_samples=OFF
 			-Dgtest_build_tests=OFF
