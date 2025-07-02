60242c043e86a45cd801d26642b7956728487831
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2025-07-02 07:18:00 +0200

    Remove license files.

--- source/ports/py_port/setup.py
+++ source/ports/py_port/setup.py
@@ -54,7 +54,6 @@ options = {
 
 	# License
 	'license': 'Apache License 2.0',
-	'license_files': ['LICENSE.txt'],
 
 	# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
 	'classifiers': [
