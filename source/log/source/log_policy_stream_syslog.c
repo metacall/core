2486ecf1afabbb3f7ca4914161c92e9abca1d3a2
Somsubhra-Nandi <nruma325@gmail.com>
2026-01-02 19:34:32 +0530

    Fix MinGW build: const-correctness for ReportEventA

--- source/log/source/log_policy_stream_syslog.c
+++ source/log/source/log_policy_stream_syslog.c
@@ -123,15 +123,19 @@ static int log_policy_stream_syslog_write(log_policy policy, const void *buffer,
 	(void)size;
 
 #if defined(_WIN32)
-	LPTSTR lpt_str[1];
+	LPCSTR lpt_str[1];
 
-	lpt_str[0] = (LPTSTR)buffer;
+	lpt_str[0] = (LPCSTR)buffer;
 
-	ReportEvent(syslog_data->handle,
+	ReportEventA(syslog_data->handle,
 		EVENTLOG_INFORMATION_TYPE,
 		LOG_POLICY_STREAM_SYSLOG_WIN_CATEGORY,
 		LOG_POLICY_STREAM_SYSLOG_WIN_MSG,
-		NULL, 1, 0, (LPTSTR *)lpt_str, NULL);
+		NULL,
+		1,
+		0,
+		lpt_str,
+		NULL);
 #elif defined(linux) || defined(__linux__) || defined(__linux) || defined(__gnu_linux) || \
 	defined(__FreeBSD__) || \
 	(defined(__APPLE__) && defined(__MACH__)) || defined(__MACOSX__)
