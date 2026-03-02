d64dd2082fe4b9beb8f3b6efdf713eb904d21a81
Abhiswant Chaudhary <abhiswant0324@gmail.com>
2026-03-03 00:14:37 +0530

    Add VxWorks and QNX platform support for dynlink (#232)
    Signed-off-by: Abhiswant Chaudhary <abhiswant0324@gmail.com>

---
+++ source/dynlink/source/dynlink_impl_qnx.c
@@ -0,0 +1,158 @@
+/*
+ *	Dynamic Link Library by Parra Studios
+ *	A library for dynamic loading and linking shared objects at run-time.
+ *
+ *	Copyright (C) 2016 - 2026 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ *
+ */
+
+/* -- Headers -- */
+
+#include <dynlink/dynlink.h>
+
+#include <dynlink/dynlink_impl.h>
+
+#include <log/log.h>
+
+#include <string.h>
+
+#include <dlfcn.h>
+
+/* -- Methods -- */
+
+const char *dynlink_impl_interface_prefix_qnx(void)
+{
+	static const char prefix_qnx[] = "lib";
+
+	return prefix_qnx;
+}
+
+const char *dynlink_impl_interface_extension_qnx(void)
+{
+	static const char extension_qnx[] = "so";
+
+	return extension_qnx;
+}
+
+dynlink_impl dynlink_impl_interface_load_qnx(dynlink handle)
+{
+	dynlink_flags flags = dynlink_get_flags(handle);
+	int flags_impl;
+	void *impl;
+
+	DYNLINK_FLAGS_SET(flags_impl, 0);
+
+	/* QNX supports standard POSIX dlopen flags */
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_NOW))
+	{
+		DYNLINK_FLAGS_ADD(flags_impl, RTLD_NOW);
+	}
+
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_LAZY))
+	{
+		DYNLINK_FLAGS_ADD(flags_impl, RTLD_LAZY);
+	}
+
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_LOCAL))
+	{
+		DYNLINK_FLAGS_ADD(flags_impl, RTLD_LOCAL);
+	}
+
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_GLOBAL))
+	{
+		DYNLINK_FLAGS_ADD(flags_impl, RTLD_GLOBAL);
+	}
+
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_SELF))
+	{
+		impl = dlopen(NULL, flags_impl);
+	}
+	else
+	{
+		impl = dlopen(dynlink_get_path(handle), flags_impl);
+	}
+
+	if (impl == NULL)
+	{
+		log_write("metacall", LOG_LEVEL_ERROR, "DynLink QNX error: %s", dlerror());
+
+		return NULL;
+	}
+
+	return (dynlink_impl)impl;
+}
+
+int dynlink_impl_interface_symbol_qnx(dynlink handle, dynlink_impl impl, const char *name, dynlink_symbol_addr *addr)
+{
+	void *symbol;
+	const char *err;
+
+	(void)handle;
+
+	/* Clear any existing error state before calling dlsym */
+	dlerror();
+
+	symbol = dlsym(impl, name);
+
+	/* Check if dlsym failed by checking dlerror after the call.
+	 * Note: dlsym can legitimately return NULL for a symbol that has NULL address,
+	 * so we must use dlerror() to distinguish between failure and NULL address. */
+	err = dlerror();
+
+	if (err != NULL)
+	{
+		log_write("metacall", LOG_LEVEL_DEBUG, "DynLink QNX: Symbol '%s' not found: %s", name, err);
+		*addr = NULL;
+		return 1;
+	}
+
+	dynlink_symbol_cast(void *, symbol, *addr);
+
+	return (*addr == NULL);
+}
+
+int dynlink_impl_interface_unload_qnx(dynlink handle, dynlink_impl impl)
+{
+	dynlink_flags flags = dynlink_get_flags(handle);
+
+	(void)handle;
+
+	/* Skip unlink when using global handle for loading symbols of the current process */
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_SELF))
+	{
+		return 0;
+	}
+
+#if defined(__MEMORYCHECK__) || defined(__ADDRESS_SANITIZER__) || defined(__THREAD_SANITIZER__) || defined(__MEMORY_SANITIZER__)
+	/* Disable dlclose when running with valgrind or sanitizers in order to maintain stacktraces */
+	(void)impl;
+	return 0;
+#else
+	return dlclose(impl);
+#endif
+}
+
+dynlink_impl_interface dynlink_impl_interface_singleton(void)
+{
+	static struct dynlink_impl_interface_type impl_interface_qnx = {
+		&dynlink_impl_interface_prefix_qnx,
+		&dynlink_impl_interface_extension_qnx,
+		&dynlink_impl_interface_load_qnx,
+		&dynlink_impl_interface_symbol_qnx,
+		&dynlink_impl_interface_unload_qnx,
+	};
+
+	return &impl_interface_qnx;
+}
---
+++ source/dynlink/source/dynlink_impl_vxworks.c
@@ -0,0 +1,189 @@
+/*
+ *	Dynamic Link Library by Parra Studios
+ *	A library for dynamic loading and linking shared objects at run-time.
+ *
+ *	Copyright (C) 2016 - 2026 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ *
+ */
+
+/* -- Headers -- */
+
+#include <dynlink/dynlink.h>
+
+#include <dynlink/dynlink_impl.h>
+
+#include <log/log.h>
+
+#include <string.h>
+
+#include <fcntl.h>
+#include <ioLib.h>
+#include <loadLib.h>
+#include <symLib.h>
+#include <sysSymTbl.h>
+#include <unldLib.h>
+#include <errnoLib.h>
+
+/* -- Methods -- */
+
+const char *dynlink_impl_interface_prefix_vxworks(void)
+{
+	/* VxWorks DKM (Downloadable Kernel Module) mode does not use lib prefix.
+	 * Files are loaded as mymodule.so or mymodule.out directly. */
+	static const char prefix_vxworks[] = "";
+
+	return prefix_vxworks;
+}
+
+const char *dynlink_impl_interface_extension_vxworks(void)
+{
+	/* VxWorks uses different extensions:
+	 * - .out for downloadable kernel modules (DKMs)
+	 * - .so for Real-Time Processes (RTPs)
+	 * We default to .so as it's more common in modern VxWorks
+	 */
+	static const char extension_vxworks[] = "so";
+
+	return extension_vxworks;
+}
+
+dynlink_impl dynlink_impl_interface_load_vxworks(dynlink handle)
+{
+	dynlink_flags flags = dynlink_get_flags(handle);
+	MODULE_ID impl;
+	int flags_impl;
+	int fd;
+
+	/* VxWorks loadModule flags:
+	 * LOAD_GLOBAL_SYMBOLS - make symbols globally available
+	 * LOAD_LOCAL_SYMBOLS - keep symbols local to the module
+	 */
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_GLOBAL))
+	{
+		DYNLINK_FLAGS_SET(flags_impl, LOAD_GLOBAL_SYMBOLS);
+	}
+	else if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_LOCAL))
+	{
+		DYNLINK_FLAGS_SET(flags_impl, LOAD_LOCAL_SYMBOLS);
+	}
+	else
+	{
+		/* Default to global symbols for compatibility */
+		DYNLINK_FLAGS_SET(flags_impl, LOAD_GLOBAL_SYMBOLS);
+	}
+
+	/* Check if we're loading symbols from the current executable */
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_SELF))
+	{
+		/* Return a special marker to indicate system symbol table usage
+		 * We use (MODULE_ID)-1 as a special handle for system symbols */
+		impl = (MODULE_ID)-1;
+	}
+	else
+	{
+		fd = open(dynlink_get_path(handle), O_RDONLY, 0);
+
+		if (fd < 0)
+		{
+			log_write("metacall", LOG_LEVEL_ERROR,
+				"DynLink VxWorks error: Failed to open module '%s' (errno: %d)",
+				dynlink_get_path(handle), errnoGet());
+
+			return NULL;
+		}
+
+		impl = loadModule(fd, flags_impl);
+
+		if (close(fd) == ERROR)
+		{
+			log_write("metacall", LOG_LEVEL_DEBUG,
+				"DynLink VxWorks warning: Failed to close module fd for '%s' (errno: %d)",
+				dynlink_get_path(handle), errnoGet());
+		}
+
+		if (impl == NULL)
+		{
+			log_write("metacall", LOG_LEVEL_ERROR,
+				"DynLink VxWorks error: Failed to load module '%s' (errno: %d)",
+				dynlink_get_path(handle), errnoGet());
+
+			return NULL;
+		}
+	}
+
+	return (dynlink_impl)impl;
+}
+
+int dynlink_impl_interface_symbol_vxworks(dynlink handle, dynlink_impl impl, const char *name, dynlink_symbol_addr *addr)
+{
+	SYM_TYPE symType;
+	void *symbol = NULL;
+	STATUS status;
+
+	(void)handle;
+	(void)impl;
+
+	/* All symbols in VxWorks are stored in the system symbol table (sysSymTbl).
+	 * There is no per-module symbol table API in the vxWorks loadModule interface. */
+	status = symFindByName(sysSymTbl, (char *)name, (char **)&symbol, &symType);
+
+	if (status != OK || symbol == NULL)
+	{
+		log_write("metacall", LOG_LEVEL_DEBUG,
+			"DynLink VxWorks: Symbol '%s' not found (errno: %d)", name, errnoGet());
+		*addr = NULL;
+		return 1;
+	}
+
+	dynlink_symbol_cast(void *, symbol, *addr);
+
+	return 0;
+}
+
+int dynlink_impl_interface_unload_vxworks(dynlink handle, dynlink_impl impl)
+{
+	dynlink_flags flags = dynlink_get_flags(handle);
+
+	/* Skip unload when using system symbol table for the current process */
+	if (DYNLINK_FLAGS_CHECK(flags, DYNLINK_FLAGS_BIND_SELF))
+	{
+		return 0;
+	}
+
+#if defined(__MEMORYCHECK__) || defined(__ADDRESS_SANITIZER__) || defined(__THREAD_SANITIZER__) || defined(__MEMORY_SANITIZER__)
+	/* Disable unload when running with sanitizers in order to maintain stacktraces */
+	(void)impl;
+	return 0;
+#else
+	/* VxWorks unldByModuleId returns OK (0) on success, ERROR (-1) on failure */
+	{
+		STATUS status = unldByModuleId((MODULE_ID)impl, 0);
+		return (status == OK) ? 0 : 1;
+	}
+#endif
+}
+
+dynlink_impl_interface dynlink_impl_interface_singleton(void)
+{
+	static struct dynlink_impl_interface_type impl_interface_vxworks = {
+		&dynlink_impl_interface_prefix_vxworks,
+		&dynlink_impl_interface_extension_vxworks,
+		&dynlink_impl_interface_load_vxworks,
+		&dynlink_impl_interface_symbol_vxworks,
+		&dynlink_impl_interface_unload_vxworks,
+	};
+
+	return &impl_interface_vxworks;
+}
