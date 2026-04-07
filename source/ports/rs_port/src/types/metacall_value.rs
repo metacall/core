1c090071cf4e6c3b004152d989bb8db47be06916
Harsh <harshdevid23@gmail.com>
2026-04-07 23:36:42 +0530

    ci: add cargo fmt --check to Rust CI pipeline (#747)
    * ci: add cargo fmt --check to Rust CI pipeline
    
    * ci: run rustfmt check in test job
    
    * ci: trigger workflow run
    
    * style: fix cargo fmt formatting in rs_port
    
    * ci: run rust fmt check after rs_port build
    
    * style: exclude auto-generated bindings.rs from rustfmt and fix metacall_value.rs formatting
    
    * fix: use outer attribute in bindgen raw_line to fix build error
    
    * ci: add rustfmt.toml to pin Rust edition for consistent formatting

--- source/ports/rs_port/src/types/metacall_value.rs
+++ source/ports/rs_port/src/types/metacall_value.rs
@@ -168,7 +168,7 @@ impl MetaCallValue for i64 {
         // TODO: This issue happens because we do not have a clear type definition in the Core
         // We are not sure yet if we should use fixed sizes in the core, or adapt all the loaders
         // and ports to the standard C int type definition. We should define this but it's not yet.
-        // Meanwhile as a workaround, we just panic if it does not fit.        
+        // Meanwhile as a workaround, we just panic if it does not fit.
         #[cfg(any(target_pointer_width = "32", windows))]
         {
             unsafe { metacall_value_create_long(self.try_into().unwrap()) }
@@ -177,7 +177,6 @@ impl MetaCallValue for i64 {
         {
             unsafe { metacall_value_create_long(self) }
         }
-
     }
 }
 /// Equivalent to MetaCall float type.
