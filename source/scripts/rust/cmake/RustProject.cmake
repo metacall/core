edb5a37af1b736a0b40516e8db5ee3a24bdd6922
yug <yugagarwal105@gmail.com>
2026-04-19 02:45:01 +0530

    minor bug

--- source/scripts/rust/cmake/RustProject.cmake
+++ source/scripts/rust/cmake/RustProject.cmake
@@ -91,7 +91,7 @@ function(rust_package target version script)
 		# Fix the version of rustc
 		COMMAND ${Rust_RUSTUP_EXECUTABLE} default nightly-2021-12-04
 		COMMAND ${Rust_RUSTC_EXECUTABLE} --crate-type=lib ${CMAKE_CURRENT_SOURCE_DIR}/source/${script}.rs --out-dir ${PROJECT_OUTPUT_DIR}
-		COMMAND ${Rust_RUSTC_EXECUTABLE} ${RUST_EXTRA_FLAGS} -verbose --crate-type=dylib -Cprefer-dynamic ${CMAKE_CURRENT_SOURCE_DIR}/source/${script}.rs --out-dir ${PROJECT_OUTPUT_DIR}
+		COMMAND ${Rust_RUSTC_EXECUTABLE} ${RUST_EXTRA_FLAGS} --verbose --crate-type=dylib -Cprefer-dynamic ${CMAKE_CURRENT_SOURCE_DIR}/source/${script}.rs --out-dir ${PROJECT_OUTPUT_DIR}
 	)
 
 	# Include generated project file
