a6265140a002bbfbfedd23fd9993f074e3b73ab2
yug <yugagarwal105@gmail.com>
2026-04-21 08:57:47 +0530

    experimenting

--- source/detours/plthook_detour/source/plthook_detour_impl.c
+++ source/detours/plthook_detour/source/plthook_detour_impl.c
@@ -30,7 +30,7 @@ int plthook_detour_impl_initialize_file(detour_impl_handle *handle, const char *
 	if (result != PLTHOOK_SUCCESS)
 	{
 		*handle = NULL;
-		log_write("metacall", LOG_LEVEL_ERROR, "PLTHook failed to open: %s", plthook_error());
+		log_write("metacall", LOG_LEVEL_ERROR, "PLTHook failed to open (code %d, path %s): %s", result, path ? path : "NULL", plthook_error());
 		return result;
 	}
 
