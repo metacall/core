f40c963b3e2836e88faeb58f96bb584d84496f1f
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2026-05-08 23:35:18 +0200

    Improve rust install.

--- tools/docker/tests/scripts/index.js
+++ tools/docker/tests/scripts/index.js
@@ -1,11 +1,26 @@
 const { metacall, metacall_load_from_file } = require('metacall');
 const assert = require('node:assert').strict;
 
-metacall_load_from_file('c', ['./bench.c']);
+// C
+(() => {
+	metacall_load_from_file('c', ['./bench.c']);
 
-const result = metacall('bench_xyz789', 1234, 15);
+	const result = metacall('bench_xyz789', 1234, 15);
 
-console.log(result);
+	console.log(result);
 
-assert.ok(typeof result === 'number', `Expected float, but got ${typeof result}`);
-assert.notStrictEqual(result, 0.0, `Expected non-zero, but got ${result}`);
+	assert.ok(typeof result === 'number', `Expected float, but got ${typeof result}`);
+	assert.notStrictEqual(result, 0.0, `Expected non-zero, but got ${result}`);
+})();
+
+// Rust
+(() => {
+	metacall_load_from_file('rs', ['./bench.rs']);
+
+	const result = metacall('pairwise_suffix_sum', [4.4, 5.5, 6.6, 7.7]);
+
+	console.log(result);
+
+	assert.ok(typeof result === 'number', `Expected float, but got ${typeof result}`);
+	assert.strictEqual(result, 0.0, `Expected non-zero, but got ${result}`);
+})();
--- tools/docker/tests/scripts/index.py
+++ tools/docker/tests/scripts/index.py
@@ -1,5 +1,6 @@
 from metacall import metacall, metacall_load_from_file
 
+# C
 assert metacall_load_from_file('c', ['./bench.c'])
 
 result = metacall('bench_xyz789', 1234, 15)
@@ -7,3 +8,12 @@ result = metacall('bench_xyz789', 1234, 15)
 print(result)
 
 assert isinstance(result, float) and result != 0.0, f"Expected non-zero float, but got {type(result)} with value {result}"
+
+# Rust
+assert metacall_load_from_file('rs', ['./bench.rs'])
+
+result = metacall('pairwise_suffix_sum', [4.4, 5.5, 6.6, 7.7])
+
+print(result)
+
+assert isinstance(result, float) and result == 0.0, f"Expected non-zero float, but got {type(result)} with value {result}"
