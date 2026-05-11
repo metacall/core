023ac64bcdfef287ece781212f56ecbaec03202d
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2026-05-11 18:34:44 +0200

    Workaround tests.

--- tools/docker/tests/scripts/index.js
+++ tools/docker/tests/scripts/index.js
@@ -1,5 +1,8 @@
 const { metacall, metacall_load_from_file } = require('metacall');
 const assert = require('node:assert').strict;
+const { promisify } = require('node:util');
+const { execFile } = require('node:child_process');
+const execFilePromises = promisify(execFile);
 
 // C
 (() => {
@@ -14,9 +17,13 @@ const assert = require('node:assert').strict;
 })();
 
 // Rust
-(() => {
-	if (process.arch === 'x64') {
-		console.log(`TODO: Rust not working for ${process.arch} architecture.`);
+(async () => {
+	// TODO: Remove this
+	const { stdout } = await execFilePromises('getconf', ['LONG_BIT']);
+	const bits = stdout.trim();
+
+	if (process.arch !== 'x64' || bits !== '64') {
+		console.log(`TODO: Rust not working for ${process.arch} architecture with ${bits} bits.`);
 		return;
 	}
 
@@ -27,5 +34,5 @@ const assert = require('node:assert').strict;
 	console.log(result);
 
 	assert.ok(typeof result === 'number', `Expected float, but got ${typeof result}`);
-	assert.notStrictEqual(result, 0.0, `Expected non-zero, but got ${result}`);
+	assert.strictEqual(result, 369.04999999999995);
 })();
