c7491bcb42104be84f9b2d2638932e75f7a6098c
yug <yugagarwal105@gmail.com>
2026-06-10 01:21:58 +0530

    haiku os

--- source/format/include/format/format_specifier.h
+++ source/format/include/format/format_specifier.h
@@ -38,7 +38,7 @@ extern "C" {
 	(defined(unix) || defined(__unix__) || defined(__unix) || \
 		defined(linux) || defined(__linux__) || defined(__linux) || defined(__gnu_linux) || \
 		defined(__CYGWIN__) || defined(__CYGWIN32__) || \
-		(defined(__APPLE__) && defined(__MACH__)) || defined(__MACOSX__))
+		(defined(__APPLE__) && defined(__MACH__)) || defined(__MACOSX__)) || defined(__HAIKU__) || defined(__BEOS__)
 
 	#include <unistd.h>
 
