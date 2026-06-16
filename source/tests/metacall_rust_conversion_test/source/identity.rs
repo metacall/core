3cc44cff183b03f0e6a489a30c4462a2d49ecf3d
SATVIKsynopsis <futureiitianisme@gmail.com>
2026-06-16 15:30:30 +0530

    Add Rust conversion tests

---
+++ source/tests/metacall_rust_conversion_test/source/identity.rs
@@ -0,0 +1,8 @@
+//fn identity_bool(x: bool) -> bool { x }
+fn identity_char(x: i8) -> i8 { x }
+fn identity_short(x: i16) -> i16 { x }
+fn identity_int(x: i32) -> i32 { x }
+fn identity_long(x: i64) -> i64 { x }
+fn identity_float(x: f32) -> f32 { x }
+fn identity_double(x: f64) -> f64 { x }
+fn identity_string(x: String) -> String { x }
