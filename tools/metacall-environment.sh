6e8918b1a8d6d9436b3e371a6e0424ad5ab0e5d1
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2026-06-17 23:55:26 +0200

    Add supressions for python instrumentation.

--- tools/metacall-environment.sh
+++ tools/metacall-environment.sh
@@ -184,7 +184,8 @@ sub_python(){
 
 				# Build Python with instrumentation
 				if [ $INSTALL_MEMCHECK = 1 ]; then
-					export ASAN_OPTIONS="halt_on_error=0:use_sigaltstack=0:detect_leaks=0"
+					printf "leak:*libpython*" &> ./asan.supp
+					export ASAN_OPTIONS="halt_on_error=0:use_sigaltstack=0:detect_leaks=0:suppressions=$(pwd)/asan.supp"
 					export UBSAN_OPTIONS="halt_on_error=0:use_sigaltstack=0"
 					sed -i 's|\/\* #define Py_USING_MEMORY_DEBUGGER \*\/|#define Py_USING_MEMORY_DEBUGGER|' Objects/obmalloc.c
 					BUILD_FLAGS="--with-valgrind"
