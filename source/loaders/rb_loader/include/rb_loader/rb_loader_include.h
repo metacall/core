032a383faec0cadb991f70dfa521d93f0cf38793
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2026-07-20 20:16:34 +0200

    Omit more warnings on rb_loader.

--- source/loaders/rb_loader/include/rb_loader/rb_loader_include.h
+++ source/loaders/rb_loader/include/rb_loader/rb_loader_include.h
@@ -30,6 +30,8 @@
 	#pragma clang diagnostic push
 	#pragma clang diagnostic ignored "-Wunused-parameter"
 	#pragma clang diagnostic ignored "-Wc23-extensions"
+	#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+	#pragma clang diagnostic ignored "-Wstrict-prototypes"
 #elif defined(__GNUC__)
 	#pragma GCC diagnostic push
 	#pragma GCC diagnostic ignored "-Wredundant-decls"
