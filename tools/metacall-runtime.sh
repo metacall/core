d5285d6636f40b41aba6a67aa8bfd563c6df22d1
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2026-08-05 08:40:11 +0200

    Add libcurl for forky and improve lsan reports.

--- tools/metacall-runtime.sh
+++ tools/metacall-runtime.sh
@@ -274,7 +274,7 @@ sub_rpc(){
 					;;
 			esac
 
-			if [ "${CODENAME}" = "trixie" ] || [ "${CODENAME}" = "noble" ] || [ "${CODENAME}" = "unstable" ]; then
+			if [ "${CODENAME}" = "forky" ] || [ "${CODENAME}" = "trixie" ] || [ "${CODENAME}" = "noble" ] || [ "${CODENAME}" = "unstable" ]; then
 				sub_apt_install_hold libcurl4t64
 			else
 				sub_apt_install_hold libcurl4
