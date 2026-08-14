df3a604be4c44be8355172bac3ff512197266fbf
Satvik Upadhyaya <futureiitianisme@gmail.com>
2026-08-14 20:56:14 +0530

    clang and realted changes

--- source/reflect/source/reflect_template.c
+++ source/reflect/source/reflect_template.c
@@ -1,18 +1,18 @@
-#include <reflect/reflect_template.h>
 #include <adt/adt_vector.h>
+#include <reflect/reflect_template.h>
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 struct reflect_template_type
 {
 	char *name;
 	template_type_id type;
-    vector parameters;
+	vector parameters;
 };
 
-reflect_template reflect_template_create(const char *name, template_type_id type)
+reflect_template template_create(const char *name, template_type_id type)
 {
 	reflect_template tpl = malloc(sizeof(struct reflect_template_type));
 
@@ -23,25 +23,24 @@ reflect_template reflect_template_create(const char *name, template_type_id type
 
 	tpl->name = strdup(name);
 	tpl->type = type;
-    tpl->parameters = vector_create(sizeof(char *));
+	tpl->parameters = vector_create(sizeof(char *));
 
 	return tpl;
 }
 
-const char *reflect_template_name(reflect_template tpl)
+const char *template_name(reflect_template tpl)
 {
 	return tpl != NULL ? tpl->name : NULL;
 }
 
-template_type_id reflect_template_type(reflect_template tpl)
+template_type_id template_type(reflect_template tpl)
 {
 	return tpl != NULL ? tpl->type : 0;
 }
 
-int reflect_template_add_parameter(
+int template_add_parameter(
 	reflect_template tpl,
-	const char *parameter
-)
+	const char *parameter)
 {
 	if (tpl == NULL || parameter == NULL)
 	{
@@ -52,13 +51,12 @@ int reflect_template_add_parameter(
 
 	vector_push_back(
 		tpl->parameters,
-		&copy
-	);
+		&copy);
 
 	return 0;
 }
 
-size_t reflect_template_parameter_count(reflect_template tpl)
+size_t template_parameter_count(reflect_template tpl)
 {
 	if (tpl == NULL)
 	{
@@ -68,11 +66,9 @@ size_t reflect_template_parameter_count(reflect_template tpl)
 	return vector_size(tpl->parameters);
 }
 
-
-const char *reflect_template_parameter(
+const char *template_parameter(
 	reflect_template tpl,
-	size_t index
-)
+	size_t index)
 {
 	if (tpl == NULL || index >= vector_size(tpl->parameters))
 	{
@@ -84,64 +80,64 @@ const char *reflect_template_parameter(
 	return parameter != NULL ? *parameter : NULL;
 }
 
-function reflect_template_instantiate_function(reflect_template tpl, template_argument args[], size_t size) {
+function template_instantiate_function(reflect_template tpl, template_argument args[], size_t size)
+{
+	if (tpl == NULL || tpl->type != TEMPLATE_TYPE_FUNCTION)
+	{
+		return NULL;
+	}
 
-    if (tpl == NULL || tpl->type != TEMPLATE_TYPE_FUNCTION) {
-        return NULL;
-    }
-
-	if (size != vector_size(tpl->parameters)) {
+	if (size != vector_size(tpl->parameters))
+	{
 		return NULL;
 	}
 
 	if (args == NULL || size == 0)
-{
-    return NULL;
-}
+	{
+		return NULL;
+	}
 
-const char *parameter = reflect_template_parameter(tpl, 0);
+	const char *parameter = template_parameter(tpl, 0);
 
-if (parameter == NULL || args[0].name == NULL)
-{
-    return NULL;
-}
+	if (parameter == NULL || args[0].name == NULL)
+	{
+		return NULL;
+	}
 
-	if (strcmp(parameter, args[0].name) != 0) {
+	if (strcmp(parameter, args[0].name) != 0)
+	{
 		return NULL;
 	}
 
 	if (args[0].value_type == NULL)
-{
-    return NULL;
+	{
+		return NULL;
+	}
+
+	char function_name[256];
+
+	snprintf(
+		function_name,
+		sizeof(function_name),
+		"%s<%s>",
+		tpl->name,
+		type_name(args[0].value_type));
+
+	function f = function_create(function_name, 1, NULL, NULL);
+
+	if (f != NULL)
+	{
+		signature_set(
+			function_signature(f),
+			0,
+			"arg",
+			args[0].value_type);
+	}
+
+	return f;
 }
 
-char function_name[256];
-
-snprintf(
-    function_name,
-    sizeof(function_name),
-    "%s<%s>",
-    tpl->name,
-    type_name(args[0].value_type)
-);
-
-function f = function_create(function_name, 1, NULL, NULL);
-
-if (f != NULL)
-{
-    signature_set(
-        function_signature(f),
-        0,
-        "arg",
-        args[0].value_type
-    );
-}
-
-return f;
-
-}
-
-void reflect_template_destroy(reflect_template tpl)
+void template_destroy(reflect_template tpl)
 {
 	if (tpl == NULL)
 	{
--- source/tests/reflect_template_test/source/reflect_template_test.cpp
+++ source/tests/reflect_template_test/source/reflect_template_test.cpp
@@ -9,72 +9,61 @@ public:
 
 TEST_F(reflect_template_test, DefaultConstructor)
 {
-	reflect_template tpl = reflect_template_create(
+	reflect_template tpl = template_create(
 		"identity",
-		TEMPLATE_TYPE_FUNCTION
-	);
+		TEMPLATE_TYPE_FUNCTION);
 
-	EXPECT_NE((reflect_template)NULL, tpl);
+	ASSERT_NE((reflect_template)NULL, tpl);
 
 	EXPECT_EQ(
 		0,
-		reflect_template_add_parameter(tpl, "T")
-	);
+		template_add_parameter(tpl, "T"));
 
 	EXPECT_EQ(
 		1,
-		reflect_template_parameter_count(tpl)
-	);
+		template_parameter_count(tpl));
 
 	EXPECT_STREQ(
 		"T",
-		reflect_template_parameter(tpl, 0)
-	);
+		template_parameter(tpl, 0));
 
 	EXPECT_STREQ(
 		"identity",
-		reflect_template_name(tpl)
-	);
+		template_name(tpl));
 
 	EXPECT_EQ(
 		TEMPLATE_TYPE_FUNCTION,
-		reflect_template_type(tpl)
-	);
+		template_type(tpl));
 
-type int_type = type_create(
-    TYPE_INT,
-    "int",
-    NULL,
-    NULL
-);
+	type int_type = type_create(
+		TYPE_INT,
+		"int",
+		NULL,
+		NULL);
 
-EXPECT_NE((type)NULL, int_type);
+	ASSERT_NE((type)NULL, int_type);
 
-template_argument args[] =
-{
-    {
-        "T",
-        int_type
-    }
-};
+	template_argument args[] = {
+		{
+			"T",
+			int_type
+		}
+	};
 
-function f =
-    reflect_template_instantiate_function(
-        tpl,
-        args,
-        1
-    );
+	function f = template_instantiate_function(
+		tpl,
+		args,
+		1);
 
-	EXPECT_NE((function)NULL, f);
+	ASSERT_NE((function)NULL, f);
 
 	EXPECT_STREQ(
-    "identity<int>",
-    function_name(f)
-);
+		"identity<int>",
+		function_name(f));
 
 	function_destroy(f);
 
-	reflect_template_destroy(tpl);
-
 	type_destroy(int_type);
+
+	template_destroy(tpl);
 }
